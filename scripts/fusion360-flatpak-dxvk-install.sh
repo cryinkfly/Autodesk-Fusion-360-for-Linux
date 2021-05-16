931adcca28b7c9b2d226e2dc755c63882f35034a
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-05-16 11:51:04 +0200

    Update and rename fusion360-flatpak-dxvk.sh to fusion360-flatpak-dxvk-install.sh

--- scripts/fusion360-flatpak-dxvk-install.sh
+++ scripts/fusion360-flatpak-dxvk-install.sh
@@ -8,7 +8,7 @@
 # Version:      0.1
 
 # 1. Step: Install Flatpak on your system: https://flatpak.org/setup/ (More information about FLatpak: https://youtu.be/SavmR9ZtHg0)
-# 2. Step: Open a Terminal and run this command: cd Downloads && chmod +x fusion360-flatpak-dxvk.sh && sh fusion360-flatpak-dxvk.sh
+# 2. Step: Open a Terminal and run this command: cd Downloads && chmod +x fusion360-flatpak-dxvk-install.sh && sh fusion360-flatpak-dxvk-install.sh
 # 3. Step: The installation will now start and set up everything for you automatically.
 # 4. Step: Now you can use my other file "fusion360-flatpak-start.sh" for running Autodesk Fusion 360 on your system.
 # 5. Step: When you have opened Fusion 360 -> Go to preferences and in General under Graphics driver, select DirectX 9.
