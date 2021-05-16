ca1b76a4885fb0af57486d595f95dfbbe3f5ab56
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-05-16 11:20:00 +0200

    Update fusion360-start.sh

--- fusion360-start.sh
+++ fusion360-start.sh
@@ -7,7 +7,7 @@
 # Time/Date:    06:00/15.05.2021
 # Version:      0.2
 
-# 1. Step: Open a Terminal and run this command: cd Downloads && chmod +x fusion360-install.sh
+# 1. Step: Open a Terminal and run this command: cd Downloads && chmod +x fusion360-start.sh && sh fusion360-start.sh
 # 2. Step: Now you can run Autodesk Fusion 360 with this file in the future.
 
 env WINEPREFIX="/$HOME/.fusion360" wine C:\\windows\\command\\start.exe /Unix /$HOME/.fusion360/dosdevices/c:/ProgramData/Microsoft/Windows/Start\ Menu/Programs/Autodesk/Autodesk\ Fusion\ 360.lnk
