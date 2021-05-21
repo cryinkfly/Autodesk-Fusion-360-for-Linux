70745ca54eee8026b6d17447c851556799bce1ba
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-05-21 20:19:47 +0200

    Update fusion360-flatpak-start.sh
    sh fusion360-flatpak-start.sh -> bash fusion360-flatpak-start.sh

--- scripts/fusion360-flatpak-start.sh
+++ scripts/fusion360-flatpak-start.sh
@@ -4,10 +4,10 @@
 # Description:  With this file you can run Autodesk Fusion 360 on your system.
 # Author:       Steve Zabka
 # Author URI:   https://cryinkfly.de
-# Time/Date:    11:00/16.05.2021
-# Version:      0.1
+# Time/Date:    20:00/21.05.2021
+# Version:      0.2
 
-# 1. Step: Open a Terminal and run this command: cd Downloads && chmod +x fusion360-flatpak-start.sh && sh fusion360-flatpak-start.sh
+# 1. Step: Open a Terminal and run this command: cd Downloads && chmod +x fusion360-flatpak-start.sh && bash fusion360-flatpak-start.sh
 # 2. Step: Now you can run Autodesk Fusion 360 with this file in the future.
 
 flatpak run org.winehq.flatpak-proton-68-ge-1 bash &&
