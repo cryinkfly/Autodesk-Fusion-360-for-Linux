fc24a0eb4218a1e4d039b28dbb9bc875805d4078
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-05-21 19:34:51 +0200

    Auto-Detection (Ubuntu and Debian versions)
    I have configured the script so, that my script select the correct Ubuntu or Debian versions.

--- scripts/fusion360-install.sh
+++ scripts/fusion360-install.sh
@@ -4,7 +4,7 @@
 # Description:  With this file you can install Autodesk Fusion 360 on Linux.
 # Author:       Steve Zabka
 # Author URI:   https://cryinkfly.de
-# Time/Date:    16:00/21.05.2021
+# Time/Date:    19:00/21.05.2021
 # Version:      0.5
 
 # 1. Step: Open a Terminal and run this command: cd Downloads && chmod +x fusion360-install.sh && sh fusion360-install.sh
@@ -15,32 +15,63 @@
 
 
 # Find your correct package manager and install some packages (the minimum requirements), what you need for the installation of Autodesk Fusion 360!
+
+# Debian versions
 if VERB="$( which apt-get )" 2> /dev/null; then
    echo "Debian-based"
-   sudo dpkg --add-architecture i386  &&
-   wget -nc https://dl.winehq.org/wine-builds/winehq.key &&
-   sudo apt-key add winehq.key &&
-   sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' &&
    sudo apt-get update &&
    sudo apt-get upgrade &&
-   sudo apt-get install p7zip p7zip-full p7zip-rar curl winbind cabextract &&
-   sudo apt-get install --install-recommends winehq-staging
+   sudo dpkg --add-architecture i386  &&
+   wget -nc https://dl.winehq.org/wine-builds/winehq.key &&
+   sudo apt-key add winehq.key
+elif [[ $(lsb_release -rs) == "10" ]]; then
+    echo "Debian 10 based system"
+    sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/debian/ buster main'
+elif [[ $(lsb_release -rs) == "11" ]]; then
+    echo "Debian 11 based system"
+    sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/debian/ bullseye main'
+
+
+# Ubuntu, Linux Mint, ... versions
+elif [[ $(lsb_release -rs) == "20.04" ]]; then
+   echo "Ubuntu 20.04 based system"
+   sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+elif [[ $(lsb_release -rs) == "20.10" ]]; then
+   echo "Ubuntu 20.10 based system"
+   sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main'
+elif [[ $(lsb_release -rs) == "21.04" ]]; then
+   echo "Ubuntu 21.04 based system"
+   sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ hirsute main'
+elif VERB="$( which apt-get )" 2> /dev/null; then
+  sudo apt-get update &&
+  sudo apt-get upgrade &&
+  sudo apt-get install p7zip p7zip-full p7zip-rar curl winbind cabextract &&
+  sudo apt-get install --install-recommends winehq-staging
+
+
+# Fedora versions
 elif VERB="$( which dnf )" 2> /dev/null; then
    echo "RedHat-based"
-   sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
-   sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/34/winehq.repo
    sudo dnf update &&
    sudo dnf upgrade &&
+   sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
+   sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/34/winehq.repo &&
    sudo dnf install p7zip p7zip-plugins curl wine cabextract
+
+
+# Arch Linux, Manjaro, ... versions
 elif VERB="$( which pacman )" 2> /dev/null; then
    echo "Arch-based"
    sudo pacman -Syu &&
    pacman -S wine wine-mono wine_gecko
+
+
+# openSUSE Leap & Tumbleweed, SUSE Linux, ... versions
 elif VERB="$( which zypper )" 2> /dev/null; then
    echo "openSUSE-based"
    su -c 'zypper up && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper install p7zip-full curl wine'
 else
-   echo "I have no idea what I'm doing." >&2
+   echo "Non-compatible Linux distribution version was found!" >&2
    exit 1
 fi
 if [[ 1 -ne $# ]]; then
