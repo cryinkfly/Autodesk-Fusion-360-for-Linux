875da69d152641a2d42bb87b1fec62c7394e728f
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-05-21 20:57:59 +0200

    Install the command line tool "xdotool"
    This tool we need later, when we start Autodesk Fusion 360 with my script "fusion360-start.sh".
    
    For more information you can read more here: https://www.semicomplete.com/projects/xdotool/

--- scripts/fusion360-dxvk-install.sh
+++ scripts/fusion360-dxvk-install.sh
@@ -4,8 +4,8 @@
 # Description:  With this file you can install Autodesk Fusion 360 on Linux.
 # Author:       Steve Zabka
 # Author URI:   https://cryinkfly.de
-# Time/Date:    19:00/21.05.2021
-# Version:      0.4
+# Time/Date:    20:55/21.05.2021
+# Version:      0.5
 
 # 1. Step: Open a Terminal and run this command: cd Downloads && chmod +x fusion360-dxvk-install.sh && bash fusion360-dxvk-install.sh
 # 2. Step: The installation will now start and set up everything for you automatically.
@@ -45,7 +45,7 @@ elif [[ $(lsb_release -rs) == "21.04" ]]; then
 elif VERB="$( which apt-get )" 2> /dev/null; then
   sudo apt-get update &&
   sudo apt-get upgrade &&
-  sudo apt-get install p7zip p7zip-full p7zip-rar curl winbind cabextract &&
+  sudo apt-get install p7zip p7zip-full p7zip-rar curl winbind cabextract xdotool &&
   sudo apt-get install --install-recommends winehq-staging
 
 
@@ -56,20 +56,20 @@ elif VERB="$( which dnf )" 2> /dev/null; then
    sudo dnf upgrade &&
    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
    sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/34/winehq.repo &&
-   sudo dnf install p7zip p7zip-plugins curl wine cabextract
+   sudo dnf install p7zip p7zip-plugins curl wine cabextract xdotool
 
 
 # Arch Linux, Manjaro, ... versions
 elif VERB="$( which pacman )" 2> /dev/null; then
    echo "Arch-based"
    sudo pacman -Syu &&
-   pacman -S wine wine-mono wine_gecko
+   pacman -S wine wine-mono wine_gecko cabextract samba xdotool
 
 
 # openSUSE Leap & Tumbleweed, SUSE Linux, ... versions
 elif VERB="$( which zypper )" 2> /dev/null; then
    echo "openSUSE-based"
-   su -c 'zypper up && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper install p7zip-full curl wine'
+   su -c 'zypper up && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper install p7zip-full curl wine xdotool'
 else
    echo "Non-compatible Linux distribution version was found!" >&2
    exit 1
