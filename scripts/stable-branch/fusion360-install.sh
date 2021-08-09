53bdab24e595c326c821b02ef614f3e1c6493948
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-08-09 21:03:17 +0200

    Add some linux distributions
    Red Hat Enterprise Linux 8.x, Solus & Ubuntu 18.04

--- scripts/stable-branch/fusion360-install.sh
+++ scripts/stable-branch/fusion360-install.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com
 # License:      MIT
 # Copyright (c) 2020-2021
-# Time/Date:    19:00/08.08.2021
-# Version:      3.0
+# Time/Date:    21:00/09.08.2021
+# Version:      3.1
 ##############################################################################
 
 # DESCRIPTION
@@ -41,7 +41,7 @@ function install-dialog+wmctrl {
 if VERB="$( which apt-get )" 2> /dev/null; then
    echo "Debian-based"
    sudo apt-get update &&
-   sudo apt-get install dialog wmctrl
+   sudo apt-get install dialog wmctrl software-properties-common
 elif VERB="$( which dnf )" 2> /dev/null; then
    echo "RedHat-based"
    sudo dnf update &&
@@ -55,6 +55,9 @@ elif VERB="$( which zypper )" 2> /dev/null; then
 elif VERB="$( which xbps-install )" 2> /dev/null; then
    echo "Void-based"
    sudo xbps-install -Sy dialog wmctrl
+elif VERB="$( which eopkg )" 2> /dev/null; then
+   echo "Solus-based"
+   sudo eopkg install dialog wmctrl
 else
    echo "I can't find your package manager!"
    exit;
@@ -66,7 +69,7 @@ function welcome_screen {
 HEIGHT=15
 WIDTH=60
 CHOICE_HEIGHT=2
-BACKTITLE="Installation of Autodesk Fusion360 - Version 3.0"
+BACKTITLE="Installation of Autodesk Fusion360 - Version 3.1"
 TITLE="Do you wish to install Autodesk Fusion 360?"
 MENU="Choose one of the following options:"
 
@@ -94,25 +97,29 @@ esac
 
 function select_your_os {
 HEIGHT=15
-WIDTH=60
+WIDTH=200
 CHOICE_HEIGHT=10
-BACKTITLE="Installation of Autodesk Fusion360 - Version 3.0"
+BACKTITLE="Installation of Autodesk Fusion360 - Version 3.1"
 TITLE="Select your Linux distribution"
 MENU="Choose one of the following options:"
 
-OPTIONS=(1 "openSUSE Leap 15.2"
-         2 "openSUSE Leap 15.3"
-         3 "openSUSE Tumbleweed"
-         4 "Debian 10 (Buster)"
-         5 "Debian 11 (Bullseye)"
-         6 "Ubuntu 20.04"
-         7 "Ubuntu 20.10"
-         8 "Ubuntu 21.04"
-         9 "Fedora 33"
-         10 "Fedora 34"
-         11 "Manjaro 19.0 & newer"
-         12 "Arch Linux"
-         13 "Void Linux")
+OPTIONS=(1 "Arch Linux, Manjaro Linux, EndeavourOS, ..."
+         2 "Debian 10, MX Linux 19.4, Raspberry Pi Desktop, ..."
+         3 "Debian 11"
+         4 "Fedora 33"
+         5 "Fedora 34"
+         6 "openSUSE Leap 15.2"
+         7 "openSUSE Leap 15.3"
+         8 "openSUSE Tumbleweed"
+         9 "Red Hat Enterprise Linux 8.x"
+         10 "Solus"
+         11 "Ubuntu 18.04, Linux Mint 19.x, ..."
+         12 "Ubuntu 20.04, Linux Mint 20.x, Pop!_OS 20.04, ..."
+         13 "Ubuntu 20.10"
+         14 "Ubuntu 21.04, Pop!_OS 21.04, ..."
+         15 "Void Linux"
+              
+         )
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -123,68 +130,109 @@ CHOICE=$(dialog --clear \
                 2>&1 >/dev/tty)
 
 clear
-case $CHOICE in
+case $CHOICE in         
         1)
-            su -c 'zypper up && zypper rr https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper install p7zip-full curl wget wine cabextract' &&
+            
+            archlinux_1
             select_your_path
             ;;
+            
         2)
-            su -c 'zypper up && zypper rr https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.3/ wine && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.3/ wine && zypper install p7zip-full curl wget wine cabextract' &&
-            select_your_path
-            ;;
-        3)
-            su -c 'zypper up && zypper install p7zip-full curl wget wine cabextract' &&
-            select_your_path
-            ;;     
-        4)
+        
             debian_based_1 &&
             sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/debian/ buster main' &&
             debian_based_2 &&
             select_your_path
-            ;;   
-        5)
+            ;;  
+            
+        3)
+        
             debian_based_1 &&
             sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/debian/ bullseye main' &&
             debian_based_2 &&
             select_your_path
             ;;  
-        6)    
-            debian_based_1 &&
-            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' &&
-            debian_based_2 &&
-            select_your_path
-            ;;   
-        7)
-            debian_based_1 &&
-            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main' &&
-            debian_based_2 &&
-            select_your_path
-            ;;    
-        8)
-            debian_based_1 &&
-            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ hirsute main' &&
-            debian_based_2 &&
-            select_your_path
-            ;;    
-        9)
+            
+        4)
+            
             fedora_based_1 &&
             sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/33/winehq.repo &&
             fedora_based_2 &&
             select_your_path
-            ;;
-        10)
+            ;;  
+            
+        5) 
+        
             fedora_based_1 &&
             sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/34/winehq.repo &&
             fedora_based_2 &&
             select_your_path
+            ;;  
+        
+        6)
+        
+            su -c 'zypper up && zypper rr https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper install p7zip-full curl wget wine cabextract' &&
+            select_your_path
             ;;
+            
+        7)
+            
+            su -c 'zypper up && zypper rr https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.3/ wine && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.3/ wine && zypper install p7zip-full curl wget wine cabextract' &&
+            select_your_path
+            ;;  
+            
+        8)
+            
+            su -c 'zypper up && zypper install p7zip-full curl wget wine cabextract' &&
+            select_your_path
+            ;;    
+            
+        9)
+        
+            redhat-linux &&
+            select_your_path
+            ;;
+            
+        10)
+        
+            solus-linux &&
+            select_your_path
+            ;;
+            
         11) 
-            archlinux_1
+        
+            debian_based_1 &&
+            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main' &&
+            debian_based_2 &&
+            select_your_path
             ;;
+            
         12) 
-            archlinux_1
+            
+            debian_based_1 &&
+            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' &&
+            debian_based_2 &&
+            select_your_path
             ;;
+            
         13) 
+        
+            debian_based_1 &&
+            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main' &&
+            debian_based_2 &&
+            select_your_path
+            ;;
+            
+        14)
+        
+            debian_based_1 &&
+            sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ hirsute main' &&
+            debian_based_2 &&
+            select_your_path
+            ;;
+            
+        15)
+        
             void-linux &&
             select_your_path
             ;;
@@ -198,7 +246,7 @@ HEIGHT=15
 WIDTH=200
 CHOICE_HEIGHT=2
 CHOICE_WIDTH=200
-BACKTITLE="Installation of Autodesk Fusion360 - Version 3.0"
+BACKTITLE="Installation of Autodesk Fusion360 - Version 3.1"
 TITLE="Choose setup type"
 MENU="Choose the kind of setup that best suits your needs."
 
@@ -227,15 +275,15 @@ esac
 
 
 function select_your_path_custom {
-    dialog --backtitle "Installation of Autodesk Fusion360 - Version 3.0" \
+    dialog --backtitle "Installation of Autodesk Fusion360 - Version 3.1" \
     --title "Description - Configure the installation location" \
     --msgbox 'Now you have to determine where you want to install Fusion 360 and then the .fusion360 folder will be created for you automatically. For examlble you can install it on a external usb-drive: /run/media/user/usb-drive/wine/.fusion360 or you install it into your home folder: /home/YOUR-USERNAME/.wineprefixes/fusion360).' 14 200
 
-    filename=$(dialog --stdout --title "Enter the installation path for Fusion 360:" --backtitle "Installation of Autodesk Fusion360 - Version 3.0" --fselect $HOME/ 14 100)
+    filename=$(dialog --stdout --title "Enter the installation path for Fusion 360:" --backtitle "Installation of Autodesk Fusion360 - Version 3.1" --fselect $HOME/ 14 100)
 }
 
 function program_exit {
-    dialog --backtitle "Installation of Autodesk Fusion360 - Version 3.0" \
+    dialog --backtitle "Installation of Autodesk Fusion360 - Version 3.1" \
     --title "Autodesk Fusion 360 is completed." \
     --msgbox 'The installation of Autodesk Fusion 360 is completed and you can use it for your projects.' 14 200
     
@@ -243,37 +291,12 @@ function program_exit {
     exit
 }
 
-function debian_based_1 {
-    sudo apt-get update &&
-    sudo apt-get upgrade &&
-    sudo dpkg --add-architecture i386  &&
-    wget -nc https://dl.winehq.org/wine-builds/winehq.key &&
-    sudo apt-key add winehq.key
-}
-
-function debian_based_2 {
-    sudo apt-get update &&
-    sudo apt-get upgrade &&
-    sudo apt-get install p7zip p7zip-full p7zip-rar curl winbind cabextract wget &&
-    sudo apt-get install --install-recommends winehq-staging
-}
-
-function fedora_based_1 {
-    sudo dnf update &&
-    sudo dnf upgrade &&
-    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-}
-
-function fedora_based_2 {
-    sudo dnf install p7zip p7zip-plugins curl wget wine cabextract
-}
-
 function archlinux_1 {
 
 HEIGHT=15
 WIDTH=60
 CHOICE_HEIGHT=2
-BACKTITLE="Installation of Autodesk Fusion360 - Version 3.0"
+BACKTITLE="Installation of Autodesk Fusion360 - Version 3.1"
 TITLE="If you have enabled multilib repository?"
 MENU="Choose one of the following options:"
 
@@ -307,6 +330,42 @@ function archlinux_2 {
    sudo pacman -Sy wine wine-mono wine_gecko winetricks p7zip curl cabextract samba ppp
 }
    
+function debian_based_1 {
+    sudo apt-get update &&
+    sudo apt-get upgrade &&
+    sudo dpkg --add-architecture i386  &&
+    wget -nc https://dl.winehq.org/wine-builds/winehq.key &&
+    sudo apt-key add winehq.key
+}
+
+function debian_based_2 {
+    sudo apt-get update &&
+    sudo apt-get upgrade &&
+    sudo apt-get install p7zip p7zip-full p7zip-rar curl winbind cabextract wget &&
+    sudo apt-get install --install-recommends winehq-staging
+}
+
+function fedora_based_1 {
+    sudo dnf update &&
+    sudo dnf upgrade &&
+    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+}
+
+function fedora_based_2 {
+    sudo dnf install p7zip p7zip-plugins curl wget wine cabextract
+}
+
+function redhat-linux {
+   sudo subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms &&
+   sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm &&
+   sudo dnf upgrade &&
+   sudo dnf install wine
+}
+
+function solus-linux {
+   sudo eopkg install wine winetricks p7zip curl cabextract samba ppp
+}
+
 function void-linux {
    sudo xbps-install -Sy wine wine-mono wine-gecko winetricks p7zip curl cabextract samba ppp
 }
