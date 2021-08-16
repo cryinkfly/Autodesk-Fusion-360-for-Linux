17cb6f8a62c42e0f9331d4f7d07af287beeb019d
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-08-16 22:14:47 +0200

    Update octoprint-server-install.sh

--- scripts/stable-branch/octoprint-server-install.sh
+++ scripts/stable-branch/octoprint-server-install.sh
@@ -38,7 +38,8 @@ if VERB="$( which apt-get )" 2> /dev/null; then
    echo "Debian-based"
    sudo apt-get update &&
    sudo apt-get install python3-pip python3-dev python3-setuptools python3-venv git libyaml-dev build-essential &&
-   mkdir OctoPrint && cd OctoPrint &&
+   mkdir OctoPrint && 
+   cd OctoPrint &&
    python3 -m venv venv &&
    source venv/bin/activate &&
    pip install pip --upgrade &&
