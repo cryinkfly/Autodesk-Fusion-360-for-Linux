0f191bfc8a72a7dbf79ea17a915fa46553d815eb
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-09-16 16:52:23 +0200

    Update octoprint-server-install.sh

--- scripts/stable-branch/octoprint-server-install.sh
+++ scripts/stable-branch/octoprint-server-install.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com
 # License:      MIT
 # Copyright (c) 2020-2021
-# Time/Date:    22:30/16.08.2021
-# Version:      1.2
+# Time/Date:    17:00/16.09.2021
+# Version:      1.3
 ##############################################################################
 
 # DESCRIPTION
@@ -34,8 +34,6 @@ fi
 
 
 function install-requirement-check {
-if VERB="$( which apt-get )" 2> /dev/null; then
-   echo "Debian-based"
    sudo apt-get update &&
    sudo apt-get install python3-pip python3-dev python3-setuptools python3-venv git libyaml-dev build-essential &&
    mkdir OctoPrint && 
@@ -44,7 +42,7 @@ if VERB="$( which apt-get )" 2> /dev/null; then
    source venv/bin/activate &&
    pip install pip --upgrade &&
    pip install octoprint &&
-
+   
    sudo usermod -a -G tty $USER &&
    sudo usermod -a -G dialout $USER &&
 
@@ -57,10 +55,9 @@ if VERB="$( which apt-get )" 2> /dev/null; then
 
    echo "The installation of OctoPrint (Server) is completed and you can use it for your projects."
    echo "Now you can use this command for manage OctoPrint (Server) on your system: sudo service octoprint {start|stop|restart}"
-else
-   echo "I can't find your package manager!"
+
    exit;
-fi
+
 }
 
 requirement-check
