56f2daa58b2841e03a26462a2ffed1cf05632f96
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-10-19 13:23:51 +0200

    Add files via upload

---
+++ files/scripts/stable-branch/data/locale/de-DE/locale-de.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# License-checkbox
+text_license_checkbox="Ich habe die Lizenzvereinbachung gelesen und akzeptiere diese."
+
+# Select
+text_select="Wähle"
+
+# Driver
+text_driver="Treiber"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (Standard) - Wählen Sie diese Option, wenn Sie sich nicht sicher sind!"
+
+# Driver DXVK
+text_driver_dxvk="DXVK - Wählen Sie diese Option, wenn Sie eine Intel-GPU verwenden!"
+
+# Linux distribution
+text_linux_distribution="Linux Distribution"
+
+# Installation location
+text_installation_location="Installationsort"
+
+# Installation location - Standard
+text_installation_location_standard="Standard – Installieren Sie Autodesk Fusion 360 in Ihrem Benutzerordner. Beispiel: /home/IHR-BENUTZERNAME/.wineprefixes/fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Benutzerdefiniert – Installieren Sie Autodesk Fusion 360 an einem anderen Ort. Zum Beispiel: /run/media/user/usb-drive/wine/.fusion 360"
+
+# Select a location with a file browser
+text_select_location_custom="Wählen Sie einen Speicherort für Ihre Autodesk Fusion 360-Installation aus:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Ich habe den Pfad von Autodesk Fusion 360 geändert!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Möchtest du deine Änderungen speichern?"
+
+# Abort the program
+text_abort="Möchten Sie die Installation wirklich abbrechen?"
+
+# Program-Exit
+text_completed_installation="Die Installation von Autodesk Fusion 360 ist abgeschlossen."
+
+# Select a option - new_modify_deinstall
+text_select_option="Option"
+
+# Option 1
+text_select_option_1="Neuinstallation einiger oder aller Komponenten"
+
+# Option 2
+text_select_option_2="Aktualisieren oder Reparieren Sie eine vorhandene Installation"
+
+# Option 3
+text_select_option_3="Deinstallieren Sie alle Autodesk Fusion 360-Komponenten"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Ich habe jetzt gesehen, welche Installationspfade verfügbar sind und werde im nächsten Schritt einen anderen Pfad (Installationsort) angeben!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Ich habe den richtigen Pfad aus einer vorhandenen Autodesk Fusion 360-Installation aufgeschrieben oder in die Zwischenablage kopiert!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Ich habe den richtigen Pfad aus einer bestehenden Autodesk Fusion 360-Installation aufgeschrieben oder in die Zwischenablage kopiert und diesen Pfad dann hier gelöscht!"
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Möchten Sie Ihre Änderungen speichern und die vorhandene Autodesk Fusion 360-Installation wirklich löschen?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Wählen Sie Ihre vorhandene Autodesk Fusion 360-Installation aus:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Die Deinstallation von Autodesk Fusion 360 ist abgeschlossen."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Wählen Sie die Installationsdatei aus:"
+
+# No Plugin was not found
+text_info_czech_plugin="Keine Datei ausgewählt."
+
+# Extension
+text_extension="Erweiterung"
+
+# Extension Description
+text_extension_description="Beschreibung"
+
+text_extension_description_1="Diese Erweiterung hilft Ihnen bei der Optimierung aller Dinge oder Teile, die einem bewegten Gas oder einer Flüssigkeit ausgesetzt sind. Zum Beispiel: Flügel, Flossen, Propeller und Turbinen."
+text_extension_description_2="Diese Erweiterung hilft Ihnen, indem sie eine Reihe von Aspekten Ihrer Konstruktionen analysiert und klare Rückmeldungen liefert, wie die Herstellbarkeit des Teils verbessert werden kann."
+text_extension_description_3="Diese Erweiterung bietet Ihnen die Möglichkeit, die tschechische Benutzeroberfläche (UI) in Autodesk Fusion 360 zu verwenden."
+text_extension_description_4="Diese Erweiterung stellt eine Verbindung zwischen Autodesk® Fusion 360™ und der HP SmartStream Software her und wird verwendet, um Aufträge direkt an die HP Software zu senden."
+text_extension_description_5="Mit dieser Erweiterung können Sie den G-Code Ihrer erstellten 3D-Modelle über Autodesk Fusion 360 direkt an den OctoPrint-Server senden."
+text_extension_description_6="Mit dieser Erweiterung können Sie mehr als 50 verschiedene Roboterhersteller und 500 Roboter direkt aus Fusion 360 heraus programmieren."
+
+# Error
+text_error="Ein unerwarteter Fehler ist aufgetreten!"
