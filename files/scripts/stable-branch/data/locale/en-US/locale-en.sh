f92a8e07d93a7efaa93af24855c1a2fa84ae372c
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-10-19 12:53:03 +0200

    Add files via upload

---
+++ files/scripts/stable-branch/data/locale/en-US/locale-en.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# License-checkbox
+text_license_checkbox="I have read the terms and conditions and I accept them."
+
+# Select
+text_select="Select"
+
+# Driver
+text_driver="Driver"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (Standard) - Select this option if you are not sure!"
+
+# Driver DXVK
+text_driver_dxvk="DXVK - Select this option if you are using an Intel GPU!"
+
+# Linux distribution
+text_linux_distribution="Linux distribution"
+
+# Installation location
+text_installation_location="Installation location"
+
+# Installation location - Standard
+text_installation_location_standard="Standard - Install Autodesk Fusion 360 into your home folder. For examble: /home/YOUR-USERNAME/.wineprefixes/fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Custom - Install Autodesk Fusion 360 to another place. For examble: /run/media/user/usb-drive/wine/.fusion360"
+
+# Select a location with a file browser
+text_select_location_custom="Select a location for your Autodesk Fusion 360 installation:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="I have changed the path of Autodesk Fusion 360!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Do you want to save your changes?"
+
+# Abort the program
+text_abort="Do you really want to abort the installation?"
+
+# Program-Exit
+text_completed_installation="The installation of Autodesk Fusion 360 is completed."
+
+# Select a option - new_modify_deinstall
+text_select_option="Option"
+
+# Option 1
+text_select_option_1="New installation of some or all components"
+
+# Option 2
+text_select_option_2="Update or Repair a existing installation"
+
+# Option 3
+text_select_option_3="Uninstall all Autodesk Fusion 360 components"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="I have now seen which installation paths are available and will specify a different path in the next step!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="I wrote down or copied the correct path from an existing Autodesk Fusion 360 installation!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="I wrote down or copied the correct path from an existing Autodesk Fusion 360 installation and deleted this path then here!"
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Do you want to save your changes and deleting the correct existing Autodesk Fusion 360 installation?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Select your exist Autodesk Fusion 360 installation:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="The deinstallation of Autodesk Fusion 360 is completed."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Select the installation file:"
+
+# No Plugin was not found
+text_info_czech_plugin="No file selected."
+
+# Extension
+text_extension="Extension"
+
+# Extension Description
+text_extension_description="Description"
+
+text_extension_description_1="This extension helps you by the optimization any thing or part that is exposed to a moving gas or liquid. For examble: wings, fins, propellers and turbines."
+text_extension_description_2="This extension helps you by analyses a number of aspects of your designs and provides clear feedback on how to improve the manufacturability of the part."
+text_extension_description_3="This extension gives you the option to use the Czech user interface (UI) in Autodesk Fusion 360."
+text_extension_description_4="This extension is a connector between Autodesk® Fusion 360™ and the HP SmartStream Software and is used to send over jobs directly to the HP Software."
+text_extension_description_5="With this extension you can send the G-code of your created 3D models directly to the OctoPrint server via Autodesk Fusion 360."
+text_extension_description_6="This extension allows you to program more than 50 different robot manufacturers and 500 robots directly from Fusion 360."
+
+# Error
+text_error="An unexpected error occurred!"
