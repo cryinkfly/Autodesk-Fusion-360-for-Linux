90c46e05e2d6b6f5fc40911687caa61e161f10b6
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-10-20 09:28:08 +0200

    Add files via upload

---
+++ files/scripts/stable-branch/data/locale/it-IT/locale-it.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# License-checkbox
+text_license_checkbox="Ho letto i termini e condizioni e li accetto."
+
+# Select
+text_select="Selezionare"
+
+# Driver
+text_driver="Driver grafico"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (Standard) - Seleziona questa opzione se non sei sicuro!"
+
+# Driver DXVK
+text_driver_dxvk="DXVK: seleziona questa opzione se stai utilizzando una GPU Intel!"
+
+# Linux distribution
+text_linux_distribution="Distribuzione Linux"
+
+# Installation location
+text_installation_location="Luogo di installazione"
+
+# Installation location - Standard
+text_installation_location_standard="Standard: installa Autodesk Fusion 360 nella cartella Inizio. Ad esempio: /home/NOME-UTENTE/.wineprefixes/fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Personalizzato: installa Autodesk Fusion 360 in un'altra posizione. Ad esempio: /run/media/NOME-UTENTE/usb-drive/wineprefixes/.fusion360"
+
+# Select a location with a file browser
+text_select_location_custom="Seleziona una posizione per l'installazione di Autodesk Fusion 360:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Ho cambiato il percorso di Autodesk Fusion 360!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Vuoi salvare le tue modifiche?"
+
+# Abort the program
+text_abort="Vuoi davvero interrompere l'installazione?"
+
+# Program-Exit
+text_completed_installation="L'installazione di Autodesk Fusion 360 è completata."
+
+# Select a option - new_modify_deinstall
+text_select_option="Opzione"
+
+# Option 1
+text_select_option_1="Nuova installazione di alcuni o tutti i componenti"
+
+# Option 2
+text_select_option_2="Aggiorna o ripara un'installazione esistente"
+
+# Option 3
+text_select_option_3="Disinstallare tutti i componenti di Autodesk Fusion 360"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Ora ho visto quali percorsi di installazione sono disponibili e specificherò un percorso diverso nel passaggio successivo!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Ho annotato o copiato il percorso corretto da un'installazione esistente di Autodesk Fusion 360!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Ho annotato o copiato il percorso corretto da un'installazione esistente di Autodesk Fusion 360 e ho eliminato questo percorso quindi qui!"
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Vuoi salvare le modifiche ed eliminare l'installazione corretta di Autodesk Fusion 360 esistente?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Seleziona l'installazione esistente di Autodesk Fusion 360:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="La disinstallazione di Autodesk Fusion 360 è stata completata."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Seleziona il file di installazione:"
+
+# No Plugin was not found
+text_info_czech_plugin="Nessun file selezionato."
+
+# Extension
+text_extension="Estensione"
+
+# Extension Description
+text_extension_description="Descrizione"
+
+text_extension_description_1="Questa estensione ti aiuta a ottimizzare qualsiasi cosa o parte esposta a un gas o liquido in movimento. Ad esempio: Ali, pinne, eliche e turbine."
+text_extension_description_2="Questa estensione ti aiuta ad analizzare una serie di aspetti dei tuoi progetti e fornisce un chiaro feedback su come migliorare la producibilità della parte."
+text_extension_description_3="Questa estensione offre la possibilità di utilizzare l'interfaccia utente (UI) ceca in Autodesk Fusion 360."
+text_extension_description_4="Questa estensione è un connettore tra Autodesk® Fusion 360™ e il software HP SmartStream e viene utilizzata per inviare i lavori direttamente al software HP."
+text_extension_description_5="Con questa estensione puoi inviare il codice G dei tuoi modelli 3D creati direttamente al server OctoPrint tramite Autodesk Fusion 360."
+text_extension_description_6="Questa estensione consente di programmare più di 50 diversi produttori di robot e 500 robot direttamente da Fusion 360."
+
+# Error
+text_error="Si è verificato un errore imprevisto!"
