8f90bc805317a0d02690ce809609af525fc16a42
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-10-20 09:51:33 +0200

    Add files via upload

---
+++ files/scripts/stable-branch/data/locale/ja-JP/locale-ja.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# License-checkbox
+text_license_checkbox="Riyō kiyaku o yomi, dōi shimasu."
+
+# Select
+text_select="Sentaku suru"
+
+# Driver
+text_driver="Gurafikkudoraibā"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (hyōjun) - wakaranai baai wa, kono opushon o sentaku shite kudasai."
+
+# Driver DXVK
+text_driver_dxvk="DXVK - Intel GPU o shiyō shite iru baai wa, kono opushon o sentaku shite kudasai."
+
+# Linux distribution
+text_linux_distribution="Rinakkusu disutoribyūshon"
+
+# Installation location
+text_installation_location="Setchi basho"
+
+# Installation location - Standard
+text_installation_location_standard="Hyōjun - Autodesk Fusion 360 o hōmuforuda ni insutōru shimasu. Rei: /home/username/.wineprefixes/.fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Kasutamu - Autodesk Fusion 360 o betsu no basho ni insutōru shimasu. Rei: /run/media/username/usb-drive/.wineprefixes/.fusion360"
+
+# Select a location with a file browser
+text_select_location_custom="Autodesk Fusion 360 no insutōru basho o sentaku shimasu:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Autodesk Fusion 360 no pasu o henkō shimashita!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Henkō o hozon shimasu ka?"
+
+# Abort the program
+text_abort="Hontōni insutōru o chūshi shimasu ka?"
+
+# Program-Exit
+text_completed_installation="Autodesk Fusion 360 no insutōru ga kanryō shimashita."
+
+# Select a option - new_modify_deinstall
+text_select_option="Opushon"
+
+# Option 1
+text_select_option_1="Ichibu matawa subete no konpōnento no shinki insutōru"
+
+# Option 2
+text_select_option_2="Kizon no insutōru o kōshin matawa shūfuku suru"
+
+# Option 3
+text_select_option_3="Subete no Autodesk Fusion 360 konpōnento o an'insutōru shimasu"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Shiyō kanōna insutōrupasu o kakunin shimashita. Tsugi no suteppu de betsu no pasu o shitei shimasu."
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Kizon no Autodesk Fusion 360 insutōru kara tadashī pasu o kakitomeru ka kopī shimashita!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Kizon no Autodesk Fusion 360 insutōru kara tadashī pasu o kakitomeru ka kopī shite, kono pasu o sakujo shimashita."
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Henkō o hozon shite, kizon no tadashī Autodesk Fusion 360 insutōru o sakujo shimasu ka?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Kizon no Autodesk Fusion 360 insutōru o sentaku shimasu."
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Autodesk Fusion 360 no sakujo ga kanryō shimashita."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Insutōrufairu o sentaku shimasu:"
+
+# No Plugin was not found
+text_info_czech_plugin="Fairu ga sentaku sa rete imasen."
+
+# Extension
+text_extension="Kakudai"
+
+# Extension Description
+text_extension_description="Setsumei"
+
+text_extension_description_1="Kono kakuchō kinō wa, idō suru kitai matawa ekitai ni sarasa reru arayuru mono matawa buhin o saiteki-ka suru koto de yakudachimasu. Rei: Tsubasa, fin, puropera, tābin."
+text_extension_description_2="Kono kakuchō kinō wa, sekkei no samazamana sokumen o bunseki suru no ni yakudachi, buhin no seizō kanōsei o kaizen suru hōhō ni kansuru meikakuna fīdobakku o teikyō shimasu."
+text_extension_description_3="Kono kakuchō kinō ni yori, Autodesk Fusion 360 de Cheko-go no yūzāintāfeisu (UI) o shiyō suru opushon ga teikyō sa remasu."
+text_extension_description_4="Kono kakuchō kinō wa, Autodesk Fusion 360 to HP SmartStream sofutō~ea-kan no konekutadeari, jobu o HP sofutō~ea ni chokusetsu sōshin suru tame ni shiyō sa remasu."
+text_extension_description_5="Kono kakuchō kinō o shiyō suru to, sakusei shita 3D moderu no G kōdo o Autodesk Fusion 360 o kaishite OctoPrint sābā ni chokusetsu sōshin dekimasu."
+text_extension_description_6="Kono kakuchō kinō o shiyō suru to, 50 wokoeru samazamana robottomēkā to 500-dai no robotto o Autodesk Fusion 360 kara chokusetsu puroguramu dekimasu."
+
+# Error
+text_error="Yoki senu erā ga hassei shita!"
