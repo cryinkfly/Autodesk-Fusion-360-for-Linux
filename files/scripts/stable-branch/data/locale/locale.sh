8e5bdc281364b1da9dfc215f8208074f08800fae
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-10-20 10:39:09 +0200

    Update locale.sh

--- files/scripts/stable-branch/data/locale/locale.sh
+++ files/scripts/stable-branch/data/locale/locale.sh
@@ -1,4 +1,16 @@
+
 #!/bin/bash
+
+####################################################################################################
+# Name:         Autodesk Fusion 360 - Setup Wizard (Linux)                                         #
+# Description:  With this file you get all languages for the Setup Wizard.                         #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2021                                                                          #
+# Time/Date:    10:00/20.10.2021                                                                   #
+# Version:      1.0                                                                                #
+####################################################################################################
 
 ###############################################################################################################################################################
 # ALL FUNCTIONS ARE ARRANGED HERE:                                                                                                                            #
