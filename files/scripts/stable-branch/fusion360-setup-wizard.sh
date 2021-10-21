e4b9115b3256fd062fea41d7b0cde2ca416fdc3e
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-10-21 20:42:19 +0200

    Update fusion360-setup-wizard.sh

--- files/scripts/stable-branch/fusion360-setup-wizard.sh
+++ files/scripts/stable-branch/fusion360-setup-wizard.sh
@@ -1140,9 +1140,9 @@ function new_modify-select-opengl_dxvk {
                     TRUE "$text_driver_opengl" \
                     FALSE "$text_driver_dxvk")
 
-[[ $response = "$text_driver_opengl" ]] && driver_used=1 && select-your-path-fusion360 && . data/resources/wine/wine-custom.sh
+[[ $response = "$text_driver_opengl" ]] && driver_used=1 && select-your-path-fusion360 && winetricks-custom
 
-[[ $response = "$text_driver_dxvk" ]] && driver_used=2 && select-your-path-fusion360 && . data/resources/wine/wine-custom.sh
+[[ $response = "$text_driver_dxvk" ]] && driver_used=2 && select-your-path-fusion360 && . winetricks-custom
 
 [[ "$response" ]] || echo "Go back" && new_modify_deinstall
 }
