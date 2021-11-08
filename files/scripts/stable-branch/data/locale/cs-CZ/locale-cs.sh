798106f8f5e82d660a39890e3c3a4ccf9930379d
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-11-08 10:57:22 +0100

    Update locale-cs.sh

--- files/scripts/stable-branch/data/locale/cs-CZ/locale-cs.sh
+++ files/scripts/stable-branch/data/locale/cs-CZ/locale-cs.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2021                                                                          #
-# Time/Date:    10:00/20.10.2021                                                                   #
-# Version:      1.0                                                                                #
+# Time/Date:    10:00/08.11.2021                                                                   #
+# Version:      1.1                                                                                #
 ####################################################################################################
 
 ###############################################################################################################################################################
@@ -44,6 +44,78 @@ text_installation_location_standard="Standard - Nainstalujte si Autodesk Fusion 
 # Installation location - Custom
 text_installation_location_custom="Vlastní - Nainstalujte Autodesk Fusion 360 na jiné místo. Například: /run/media/username/usb-drive/.wineprefixes/fusion360"
 
+# Select a location with a file browser
+text_select_location_custom="Vyberte umístění pro instalaci aplikace Autodesk Fusion 360:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Změnil jsem cestu k aplikaci Autodesk Fusion 360!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Chcete uložit změny?"
+
+# Abort the program
+text_abort="Opravdu chcete instalaci přerušit?"
+
+# Program-Exit
+text_completed_installation="Instalace aplikace Autodesk Fusion 360 je dokončena."
+
+# Program-Exit (Extensions)
+text_completed_installation_extensions="Instalace některých rozšíření pro Autodesk Fusion 360 je dokončena."
+
+# Select a option - new_modify_deinstall
+text_select_option="Volba"
+
+# Option 1
+text_select_option_1="Nová instalace některých nebo všech součástí"
+
+# Option 2
+text_select_option_2="Aktualizujte nebo Opravte stávající instalaci"
+
+# Option 3
+text_select_option_3="Nainstalujte, Opravte nebo Odinstalujte některá rozšíření"
+
+# Option 4
+text_select_option_4="Odinstalujte všechny součásti Autodesk Fusion 360"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Nyní jsem viděl, které instalační cesty jsou k dispozici, a v dalším kroku zadám jinou cestu!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Poznamenal jsem si nebo zkopíroval správnou cestu z existující instalace Autodesk Fusion 360!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Poznamenal jsem si nebo zkopíroval správnou cestu z existující instalace Autodesk Fusion 360 a tuto cestu jsem pak smazal zde!"
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Chcete uložit změny a odstranit správnou stávající instalaci aplikace Autodesk Fusion 360?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Vyberte existující instalaci aplikace Autodesk Fusion 360:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Odinstalování aplikace Autodesk Fusion 360 je dokončeno."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Vyberte instalační soubor:"
+
+# No Plugin was not found
+text_info_czech_plugin="Žádný soubor nevybrán."
+
+# Extension
+text_extension="Rozšíření"
+
+# Extension Description
+text_extension_description="Popis"
+
+text_extension_description_1="Toto rozšíření vám pomůže s optimalizací jakékoli věci nebo součásti, která je vystavena pohybujícímu se plynu nebo kapalině. Například: Křídla, ploutve, vrtule a turbíny."
+text_extension_description_2="Toto rozšíření vám pomůže analyzovat řadu aspektů vašich návrhů a poskytne jasnou zpětnou vazbu o tom, jak zlepšit vyrobitelnost součásti."
+text_extension_description_3="Toto rozšíření vám dává možnost používat české uživatelské rozhraní (UI) v aplikaci Autodesk Fusion 360."
+text_extension_description_4="Toto rozšíření je konektorem mezi Autodesk Fusion 360 a softwarem HP SmartStream a slouží k odesílání úloh přímo do softwaru HP."
+text_extension_description_5="S tímto rozšířením můžete odeslat G-kód vašich vytvořených 3D modelů přímo na server OctoPrint prostřednictvím Autodesk Fusion 360."
+text_extension_description_6="Toto rozšíření vám umožňuje naprogramovat více než 50 různých výrobců robotů a 500 robotů přímo z Autodesk Fusion 360."
+
+# Error
+text_error="Vyskytla se neočekávaná chyba!"
 # Select a location with a file browser
 text_select_location_custom="Vyberte umístění pro instalaci aplikace Autodesk Fusion 360:"
 
