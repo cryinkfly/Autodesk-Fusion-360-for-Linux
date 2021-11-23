62425bfe05ce2ec1dba29b7165f79170defae559
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-11-23 09:14:16 +0100

    Update and rename launcher.sh to fusion360-launcher.sh

--- files/extras/desktop-starter/fusion360-launcher.sh
+++ files/extras/desktop-starter/fusion360-launcher.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                       #
 # License:      MIT                                                         #
 # Copyright (c) 2020-2021                                                   #
-# Time/Date:    20:00/09.10.2021                                            #
-# Version:      1.0                                                         #
+# Time/Date:    09:00/23.11.2021                                            #
+# Version:      1.1                                                         #
 #############################################################################
 
 #################################
@@ -18,6 +18,7 @@
 function fusion360-starter {
 
 # You must change the first part ($HOME/.wineprefixes/fusion360) and the last part (WINEPREFIX="$HOME/.wineprefixes/fusion360") when you have installed Autodesk Fusion 360 into another directory!
+
 launcher="$(find $HOME/.wineprefixes/fusion360 -name Fusion360.exe -printf "%T+ %p" | sort -r 2>&1 | head -n 1 | sed -r 's/.+0000000000 (.+)/\1/')" && WINEPREFIX="$HOME/.wineprefixes/fusion360" wine "$launcher"
 
 }
