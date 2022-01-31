63d7ac15e40154f384701c27a01dacfb1969a3bf
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-01-31 17:36:45 +0100

    Change desktop icon

--- files/scripts/stable-branch/fusion360-setup-wizard.sh
+++ files/scripts/stable-branch/fusion360-setup-wizard.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2021                                                                          #
-# Time/Date:    15:05/26.01.2022                                                                   #
-# Version:      1.7.5                                                                              #
+# Time/Date:    17:30/31.01.2022                                                                   #
+# Version:      1.7.6                                                                              #
 ####################################################################################################
 
 ###############################################################################################################################################################
@@ -210,7 +210,7 @@ function setupact-f360install {
    cd "$wineprefixname/drive_c/users/$USER/Application Data/Autodesk/Neutron Platform/Options"
    setupact-dxvk-opengl-3
    setupact-f360-launcher
-   wget -P $HOME/.local/share/applications/wine/Programs/Autodesk https://github.com/cryinkfly/Autodesk-Fusion-360-for-Linux/blob/main/files/extras/desktop-starter/Fusion360.png
+   wget -P $HOME/.local/share/applications/wine/Programs/Autodesk https://github.com/cryinkfly/Autodesk-Fusion-360-for-Linux/blob/main/files/extras/desktop-starter/fusion360.svg
    setupact-log-f360-path
    setupact-f360extensions
    setupact-completed
