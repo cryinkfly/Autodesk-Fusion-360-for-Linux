59af309364884974b4ea623c8023a70b87978129
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-02-10 16:47:22 +0100

    Update locale-en.sh

--- files/scripts/stable-branch/data/locale/en-US/locale-en.sh
+++ files/scripts/stable-branch/data/locale/en-US/locale-en.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2022                                                                          #
-# Time/Date:    12:30/22.01.2022                                                                   #
-# Version:      1.3                                                                                #
+# Time/Date:    16:30/10.02.2022                                                                   #
+# Version:      1.4                                                                                #
 ####################################################################################################
 
 ###############################################################################################################################################################
@@ -31,10 +31,10 @@ text_snap="Snap (Experimental)"
 text_driver="Graphics driver"
 
 # Driver OpenGL
-text_driver_opengl="OpenGL (Standard) - Select this option if you are not sure!"
+text_driver_opengl="OpenGL (Fallback-Variant)"
 
 # Driver DXVK
-text_driver_dxvk="DXVK - Select this option if you are using an Intel GPU!"
+text_driver_dxvk="DXVK (Standard-Variant)"
 
 # Linux distribution
 text_linux_distribution="Linux distribution"
