f95d1c41c8073e75fea5bea6ad1d486e4bbe5ce7
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-02-18 09:25:18 +0100

    Add files via upload

---
+++ files/scripts/stable-branch/locale/cs-CZ/locale-cs.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+####################################################################################################
+# Name:         Autodesk Fusion 360 - Setup Wizard (Linux)                                         #
+# Description:  This is the Czech translation for the Setup Wizard.                                #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2022                                                                          #
+# Time/Date:    16:30/10.02.2022                                                                   #
+# Version:      1.4                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS ARE ARRANGED HERE:                                                                                                                          #
+###############################################################################################################################################################
+
+# License-checkbox
+
+# License-checkbox
+text_license_checkbox="Přečetl jsem si podmínky a přijímám je."
+
+# Select
+text_select="Vybrat"
+
+# Version (Standard, Flatpak or Snap)
+text_version="Verze"
+text_standard="Standard"
+text_flatpak="Flatpak (Experimentální)"
+text_snap="Snap (Experimentální)"
+
+# Driver
+text_driver="Ovladač grafiky"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (Záložní Varianta)"
+
+# Driver DXVK
+text_driver_dxvk="DXVK (Standardní Varianta)"
+
+# Linux distribution
+text_linux_distribution="Distribuce Linuxu"
+
+# Installation location
+text_installation_location="Umístění instalace"
+
+# Installation location - Standard
+text_installation_location_standard="Standard - Nainstalujte si Autodesk Fusion 360 do své domovské složky. Například: /home/username/.wineprefixes/fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Vlastní - Nainstalujte Autodesk Fusion 360 na jiné místo. Například: /run/media/username/usb-drive/.wineprefixes/fusion360"
+
+# Select a location with a file browser
+text_select_location_custom="Vyberte umístění pro instalaci aplikace Autodesk Fusion 360:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Změnil jsem cestu k aplikaci Autodesk Fusion 360!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Chcete uložit změny?"
+
+# Abort the program
+text_abort="Opravdu chcete instalaci přerušit?"
+
+# Program-Exit
+text_completed_installation="Instalace aplikace Autodesk Fusion 360 je dokončena."
+
+# Program-Exit (Extensions)
+text_completed_installation_extensions="Instalace některých rozšíření pro Autodesk Fusion 360 je dokončena."
+
+# Select a option - new_modify_deinstall
+text_select_option="Volba"
+
+# Option 1
+text_select_option_1="Nová instalace některých nebo všech součástí"
+
+# Option 2
+text_select_option_2="Aktualizujte nebo Opravte stávající instalaci"
+
+# Option 3
+text_select_option_3="Nainstalujte, Opravte nebo Odinstalujte některá rozšíření"
+
+# Option 4
+text_select_option_4="Odinstalujte všechny součásti Autodesk Fusion 360"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Nyní jsem viděl, které instalační cesty jsou k dispozici, a v dalším kroku zadám jinou cestu!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Poznamenal jsem si nebo zkopíroval správnou cestu z existující instalace Autodesk Fusion 360!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Poznamenal jsem si nebo zkopíroval správnou cestu z existující instalace Autodesk Fusion 360 a tuto cestu jsem pak smazal zde!"
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Chcete uložit změny a odstranit správnou stávající instalaci aplikace Autodesk Fusion 360?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Vyberte existující instalaci aplikace Autodesk Fusion 360:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Odinstalování aplikace Autodesk Fusion 360 je dokončeno."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Vyberte instalační soubor:"
+
+# No Plugin was not found
+text_info_czech_plugin="Žádný soubor nevybrán."
+
+# Extension
+text_extension="Rozšíření"
+
+# Extension Description
+text_extension_description="Popis"
+
+text_extension_description_1="Toto rozšíření vám pomůže optimalizovat jakoukoli věc nebo část, která je vystavena pohybujícímu se plynu nebo kapalině. Například: křídla, ploutve, vrtule a turbíny."
+text_extension_description_2="Toto rozšíření vám pomůže analyzovat řadu aspektů vašich návrhů a poskytuje jasnou zpětnou vazbu, jak zlepšit vyrobitelnost dílu."
+text_extension_description_3="Toto rozšíření vám dává možnost používat české uživatelské rozhraní (UI) v Autodesk Fusion 360."
+text_extension_description_4="Toto rozšíření je konektorem mezi Autodesk® Fusion 360™ a softwarem HP SmartStream a používá se k odesílání úloh přímo do softwaru HP."
+text_extension_description_5="Šroubová ozubená kola připomínají čelní ozubená kola s šikmými zuby."
+text_extension_description_6="S tímto rozšířením můžete odeslat G-kód svých vytvořených 3D modelů přímo na server OctoPrint přes Autodesk Fusion 360."
+text_extension_description_7="Umožňuje uživateli importovat/aktualizovat parametry ze souboru CSV (hodnoty oddělené čárkami) nebo je exportovat do souboru."
+text_extension_description_8="Toto rozšíření umožňuje programovat více než 50 různých výrobců robotů a 500 robotů přímo z Autodesk Fusion 360."
+text_extension_description_9="Tento plug-in je konektorem mezi Autodesk® Fusion 360™ a webem Ultimaker Digital Factory a jeho službami."
+
+# Error
+text_error="Vyskytla se neočekávaná chyba!"
+
+
+
