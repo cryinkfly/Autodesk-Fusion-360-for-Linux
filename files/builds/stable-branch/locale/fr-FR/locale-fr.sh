ce5f05677e2d56b125d732d646a63bcba018ce74
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-02-18 09:50:44 +0100

    Add files via upload

---
+++ files/builds/stable-branch/locale/fr-FR/locale-fr.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+####################################################################################################
+# Name:         Autodesk Fusion 360 - Setup Wizard (Linux)                                         #
+# Description:  This is the French translation for the Setup Wizard.                              #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2022                                                                          #
+# Time/Date:    16:30/10.02.2022                                                                   #
+# Version:      1.4                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS ARE ARRANGED HERE:                                                                                                                          #
+###############################################################################################################################################################
+
+# License-checkbox
+text_license_checkbox="J'ai lu les termes et conditions et je les accepte."
+
+# Select
+text_select="Sélectionner"
+
+# Version (Standard, Flatpak or Snap)
+text_version="Version"
+text_standard="Standard"
+text_flatpak="Flatpak (Expérimental)"
+text_snap="Snap (Expérimental)"
+
+# Driver
+text_driver="Pilote graphique"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (Variante de repli)"
+
+# Driver DXVK
+text_driver_dxvk="DXVK (Variante standard)"
+
+# Linux distribution
+text_linux_distribution="Distribution Linux"
+
+# Installation location
+text_installation_location="Emplacement d'installation"
+
+# Installation location - Standard
+text_installation_location_standard="Standard - Installez Autodesk Fusion 360 dans votre dossier de départ. Par exemple: /home/username/.wineprefixes/fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Personnalisé - Installez Autodesk Fusion 360 à un autre endroit. Par exemple: /run/media/username/usb-drive/.wineprefixes/fusion360"
+
+# Select a location with a file browser
+text_select_location_custom="Sélectionnez un emplacement pour votre installation d'Autodesk Fusion 360:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="J'ai modifié le chemin d'accès d'Autodesk Fusion 360!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Voulez-vous enregistrer vos modifications?"
+
+# Abort the program
+text_abort="Voulez-vous vraiment interrompre l'installation?"
+
+# Program-Exit
+text_completed_installation="L'installation d'Autodesk Fusion 360 est terminée."
+
+# Program-Exit (Extensions)
+text_completed_installation_extensions="L'installation de certaines extensions pour Autodesk Fusion 360 est terminée."
+
+# Select a option - new_modify_deinstall
+text_select_option="Option"
+
+# Option 1
+text_select_option_1="Nouvelle installation de certains ou de tous les composants"
+
+# Option 2
+text_select_option_2="Mettre à jour ou réparer une installation existante"
+
+# Option 3
+text_select_option_3="Installer, Réparer ou Désinstaller certaines extensions"
+
+# Option 4
+text_select_option_4="Désinstaller tous les composants Autodesk Fusion 360"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="J'ai maintenant vu quels chemins d'installation sont disponibles et je spécifierai un chemin différent à l'étape suivante!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="J'ai noté ou copié le chemin correct à partir d'une installation Autodesk Fusion 360 existante!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="J'ai noté ou copié le chemin correct à partir d'une installation Autodesk Fusion 360 existante et supprimé ce chemin, puis ici!"
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Voulez-vous enregistrer vos modifications et supprimer l'installation Autodesk Fusion 360 existante correcte?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Sélectionnez votre installation Autodesk Fusion 360 existante:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="La désinstallation d'Autodesk Fusion 360 est terminée."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Sélectionnez le fichier d'installation:"
+
+# No Plugin was not found
+text_info_czech_plugin="Aucun fichier sélectionné."
+
+# Extension
+text_extension="Extension"
+
+# Extension Description
+text_extension_description="La description"
+
+text_extension_description_1="Cette extension vous aide par l'optimisation de toute chose ou pièce qui est exposée à un gaz ou un liquide en mouvement. Par exemple : ailes, ailettes, hélices et turbines."
+text_extension_description_2="Cette extension vous aide en analysant un certain nombre d'aspects de vos conceptions et fournit des informations claires sur la façon d'améliorer la fabricabilité de la pièce."
+text_extension_description_3="Cette extension vous donne la possibilité d'utiliser l'interface utilisateur (UI) tchèque dans Autodesk Fusion 360."
+text_extension_description_4="Cette extension est un connecteur entre Autodesk® Fusion 360™ et le logiciel HP SmartStream et est utilisée pour envoyer des travaux directement au logiciel HP."
+text_extension_description_5="Les engrenages hélicoïdaux ressemblent à des engrenages droits avec les dents inclinées."
+text_extension_description_6="Avec cette extension, vous pouvez envoyer le code G de vos modèles 3D créés directement au serveur OctoPrint via Autodesk Fusion 360."
+text_extension_description_7="Permet à l'utilisateur d'importer/mettre à jour des paramètres depuis ou de les exporter vers un fichier CSV (Comma Separated Values)."
+text_extension_description_8="Cette extension vous permet de programmer plus de 50 fabricants de robots différents et 500 robots directement depuis Autodesk Fusion 360."
+text_extension_description_9="Ce plug-in est un connecteur entre Autodesk® Fusion 360™ et le site Ultimaker Digital Factory et ses services."
+
+# Error
+text_error="Une erreur inattendue est apparue!"
+
