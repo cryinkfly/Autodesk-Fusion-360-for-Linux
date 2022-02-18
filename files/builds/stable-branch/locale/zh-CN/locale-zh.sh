14b1d9757de5b4bf5971c89951f49e348fc1557d
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-02-18 09:55:50 +0100

    Add files via upload

---
+++ files/builds/stable-branch/locale/zh-CN/locale-zh.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+####################################################################################################
+# Name:         Autodesk Fusion 360 - Setup Wizard (Linux)                                         #
+# Description:  This is the Chinese translation for the Setup Wizard.                              #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2022                                                                          #
+# Time/Date:    16:30/10.02.2022                                                                   #
+# Version:      1.4                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS ARE ARRANGED HERE:                                                                                                                          #
+###############################################################################################################################################################
+
+# License-checkbox
+text_license_checkbox="Wǒ yǐ yuèdú tiáokuǎn hé tiáojiàn bìng jiēshòu tāmen."
+
+# Select
+text_select="Xuǎnzé"
+
+# Version (Standard, Flatpak or Snap)
+text_version="Bǎnběn"
+text_standard="Biāozhǔn"
+text_flatpak="Flatpak (Shíyàn xìng de)"
+text_snap="Snap (Shíyàn xìng de)"
+
+# Driver
+text_driver="Xiǎnkǎ qūdòng"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (Hòubèi biàn tǐ)"
+
+# Driver DXVK
+text_driver_dxvk="DXVK (biāozhǔn biàn tǐ)"
+
+# Linux distribution
+text_linux_distribution="Linux fāxíng bǎn"
+
+# Installation location
+text_installation_location="Ānzhuāng wèizhì"
+
+# Installation location - Standard
+text_installation_location_standard="Biāozhǔn - Jiāng Autodesk Fusion 360 ānzhuāng dào nín de zhǔ wénjiàn jiā zhōng. Lìrú: /home/username/.wineprefixes/fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Zì dìngyì - Jiāng Autodesk Fusion 360 ānzhuāng dào qítā wèizhì. Lìrú: /run/media/username/usb-drive/.wineprefixes/fusion360"
+
+# Select a location with a file browser
+text_select_location_custom="Xuǎnzé Autodesk Fusion 360 ānzhuāng wèizhì:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Wǒ yǐjīng gēnggǎile Autodesk Fusion 360 de lùjìng!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Shìfǒu yào bǎocún gēnggǎi?"
+
+# Abort the program
+text_abort="Nín zhēn de yāo zhōngzhǐ ānzhuāng ma?"
+
+# Program-Exit
+text_completed_installation="Autodesk Fusion 360 ānzhuāng wánchéng."
+
+# Program-Exit (Extensions)
+text_completed_installation_extensions="Autodesk Fusion 360 bùfèn kuòzhǎn ānzhuāng wánchéng."
+
+# Select a option - new_modify_deinstall
+text_select_option="Xuǎnxiàng"
+
+# Option 1
+text_select_option_1="Bùfèn huò suǒyǒu zǔjiàn de quánxīn ānzhuāng"
+
+# Option 2
+text_select_option_2="Gēngxīn huò xiūfù xiàn yǒu ānzhuāng"
+
+# Option 3
+text_select_option_3="Ānzhuāng, xiūfù huò xiè zǎi yīxiē kuòzhǎn"
+
+# Option 4
+text_select_option_4="Xièzài suǒyǒu Autodesk Fusion 360 zǔjiàn"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Wǒ xiànzài yǐjīng kàn dàole nǎxiē ānzhuāng lùjìng kěyòng, bìng jiàng zàixià yībù zhōng zhǐdìng bùtóng de lùjìng!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Wǒ cóng xiàn yǒu de Autodesk Fusion 360 ānzhuāng zhōng jì xià huò fùzhìle zhèngquè de lùjìng!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Wǒ cóng xiàn yǒu de Autodesk Fusion 360 ānzhuāng zhōng jì xià huò fùzhìle zhèngquè de lùjìng, ránhòu zài cǐ chù shān chú liǎo cǐ lùjìng!"
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Shìfǒu yào bǎocún gēnggǎi bìng shānchú zhèngquè de xiàn yǒu Autodesk Fusion 360 ānzhuāng?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Xuǎnzé nín xiàn yǒu de Autodesk Fusion 360 ānzhuāng:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Autodesk Fusion 360 xièzài wánchéng."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Xuǎnzé ānzhuāng wénjiàn:"
+
+# No Plugin was not found
+text_info_czech_plugin="Wèi xuǎnzé wénjiàn."
+
+# Extension
+text_extension="Yánqí"
+
+# Extension Description
+text_extension_description="Miáoshù"
+
+text_extension_description_1="此扩展帮助您优化暴露于移动气体或液体的任何事物或部件。例如：机翼、鳍、螺旋桨和涡轮机。"
+text_extension_description_2="此扩展帮助您分析设计的多个方面，并就如何提高零件的可制造性提供清晰的反馈。"
+text_extension_description_3="此扩展使您可以选择在 Autodesk Fusion 360 中使用捷克语用户界面 (UI)。"
+text_extension_description_4="此扩展是 Autodesk® Fusion 360™ 和 HP SmartStream 软件之间的连接器，用于将作业直接发送到 HP 软件。"
+text_extension_description_5="斜齿轮类似于正齿轮，齿呈一定角度。"
+text_extension_description_6="使用此扩展，您可以通过 Autodesk Fusion 360 将您创建的 3D 模型的 G 代码直接发送到 OctoPrint 服务器。"
+text_extension_description_7="允许用户从 CSV（逗号分隔值）文件导入/更新参数或将其导出到 CSV（逗号分隔值）文件。"
+text_extension_description_8="此扩展允许您直接从 Autodesk Fusion 360 对 50 多个不同的机器人制造商和 500 个机器人进行编程。"
+text_extension_description_9="此插件是 Autodesk® Fusion 360™ 与 Ultimaker Digital Factory 站点及其服务之间的连接器。"
+
+# Error
+text_error="Yīgè yìliào zhī wài de wèntí fāshēngle!"
+
