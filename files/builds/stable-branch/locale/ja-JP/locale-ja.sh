39c329efd536586dd2422e3e78b943547629ab27
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-02-18 09:53:04 +0100

    Add files via upload

---
+++ files/builds/stable-branch/locale/ja-JP/locale-ja.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+####################################################################################################
+# Name:         Autodesk Fusion 360 - Setup Wizard (Linux)                                         #
+# Description:  This is the Japanese translation for the Setup Wizard.                             #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2022                                                                          #
+# Time/Date:    16:30/10.02.2022                                                                   #
+# Version:      1.4                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS ARE ARRANGED HERE:                                                                                                                          #
+###############################################################################################################################################################
+
+# License-checkbox
+text_license_checkbox="Riyō kiyaku o yomi, dōi shimasu."
+
+# Select
+text_select="Sentaku suru"
+
+# Version (Standard, Flatpak or Snap)
+text_version="Bājon"
+text_standard="Hyōjun"
+text_flatpak="Flatpak (Jikken-teki)"
+text_snap="Snap (Jikken-teki)"
+
+# Driver
+text_driver="Gurafikkudoraibā"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (Fōrubakkubarianto)"
+
+# Driver DXVK
+text_driver_dxvk="DXVK (Hyōjun barianto)"
+
+# Linux distribution
+text_linux_distribution="Rinakkusu disutoribyūshon"
+
+# Installation location
+text_installation_location="Setchi basho"
+
+# Installation location - Standard
+text_installation_location_standard="Hyōjun - Autodesk Fusion 360 o hōmuforuda ni insutōru shimasu. Rei: /home/username/.wineprefixes/fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Kasutamu - Autodesk Fusion 360 o betsu no basho ni insutōru shimasu. Rei: /run/media/username/usb-drive/.wineprefixes/fusion360"
+
+# Select a location with a file browser
+text_select_location_custom="Autodesk Fusion 360 no insutōru basho o sentaku shimasu:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Autodesk Fusion 360 no pasu o henkō shimashita!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Henkō o hozon shimasu ka?"
+
+# Abort the program
+text_abort="Hontōni insutōru o chūshi shimasu ka?"
+
+# Program-Exit
+text_completed_installation="Autodesk Fusion 360 no insutōru ga kanryō shimashita."
+
+# Program-Exit (Extensions)
+text_completed_installation_extensions="Autodesk Fusion 360 no ikutsu ka no kakuchō kinō no insutōru ga kanryō shimashita."
+
+# Select a option - new_modify_deinstall
+text_select_option="Opushon"
+
+# Option 1
+text_select_option_1="Ichibu matawa subete no konpōnento no shinki insutōru"
+
+# Option 2
+text_select_option_2="Kizon no insutōru o kōshin matawa shūfuku suru"
+
+# Option 3
+text_select_option_3="Ichibu no kakuchō kinō o insutōru, shūfuku, matawa an'insutōru suru"
+
+# Option 4
+text_select_option_4="Subete no Autodesk Fusion 360 konpōnento o an'insutōru shimasu"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Shiyō kanōna insutōrupasu o kakunin shimashita. Tsugi no suteppu de betsu no pasu o shitei shimasu."
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Kizon no Autodesk Fusion 360 insutōru kara tadashī pasu o kakitomeru ka kopī shimashita!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Kizon no Autodesk Fusion 360 insutōru kara tadashī pasu o kakitomeru ka kopī shite, kono pasu o sakujo shimashita."
+
+# Question - Deinstall a exist Autodesk Fusion 360 installation
+text_deinstall_question="Henkō o hozon shite, kizon no tadashī Autodesk Fusion 360 insutōru o sakujo shimasu ka?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Kizon no Autodesk Fusion 360 insutōru o sentaku shimasu:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Autodesk Fusion 360 no sakujo ga kanryō shimashita."
+
+# Select CZECH-Plugin files
+text_select_czech_plugin="Insutōrufairu o sentaku shimasu:"
+
+# No Plugin was not found
+text_info_czech_plugin="Fairu ga sentaku sa rete imasen."
+
+# Extension
+text_extension="Kakudai"
+
+# Extension Description
+text_extension_description="Setsumei"
+
+text_extension_description_1 = "この拡張機能は、移動する気体または液体にさらされるあらゆるものまたは部品を最適化するのに役立ちます。たとえば、翼、フィン、プロペラ、タービンなどです。"
+text_extension_description_2 = "この拡張機能は、設計のさまざまな側面を分析し、部品の製造可能性を改善する方法に関する明確なフィードバックを提供するのに役立ちます。"
+text_extension_description_3 = "この拡張機能は、Autodesk Fusion 360でチェコのユーザーインターフェイス（UI）を使用するオプションを提供します。"
+text_extension_description_4 = "この拡張機能は、Autodesk®Fusion360™とHP SmartStreamソフトウェア間のコネクタであり、ジョブをHPソフトウェアに直接送信するために使用されます。"
+text_extension_description_5 = "はすば歯車は、歯が斜めになっている平歯車に似ています。"
+text_extension_description_6 = "この拡張機能を使用すると、作成した3DモデルのGコードをAutodesk Fusion360経由でOctoPrintサーバーに直接送信できます。"
+text_extension_description_7 = "ユーザーがパラメーターをCSV（カンマ区切り値）ファイルからインポート/更新したり、CSVファイルにエクスポートしたりできるようにします。"
+text_extension_description_8 = "この拡張機能を使用すると、50を超えるさまざまなロボットメーカーと500のロボットをAutodesk Fusion360から直接プログラムできます。"
+text_extension_description_9 = "このプラグインは、Autodesk®Fusion360™とUltimaker DigitalFactoryサイトおよびそのサービスの間のコネクタです。"
+
+# Error
+text_error="Yoki senu erā ga hassei shita!"
+
