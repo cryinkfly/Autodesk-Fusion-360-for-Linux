ebb2cdce4bc81fd2eaf3fd448e1858fdb196256f
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-02-20 09:29:09 +0100

    Update install.sh

--- files/builds/stable-branch/bin/install.sh
+++ files/builds/stable-branch/bin/install.sh
@@ -47,21 +47,21 @@ function setupact-install-log {
 
 # Check if already exists a Autodesk Fusion 360 installation on your system.
 function setupact-check-f360 {
-f360_path="$HOME/.config/fusion-360/logs/wineprefixes.log" # Search for f360-path.log file.
-if [ -f "$f360_path" ]; then
+  f360_path="$HOME/.config/fusion-360/logs/wineprefixes.log" # Search for f360-path.log file.
+  if [ -f "$f360_path" ]; then
     cp "$f360_path" "/tmp/fusion-360/logs"
     mv "/tmp/fusion-360/logs/wineprefixes.log" "/tmp/fusion-360/logs/wineprefixes"
     setupact-modify-f360 # Modify a exists Wineprefix of Autodesk Fusion 360.
-else
+  else
     f360path_log=1
     setupact-select-f360-path # Install a new Wineprefix of Autodesk Fusion 360.
-fi
+  fi
 }
 
 # Save the path of the Wineprefix of Autodesk Fusion 360 into the f360-path.log file.
 function setupact-log-f360-path {
 if [ $f360path_log -eq 1 ]; then
-    echo "$wineprefixname" >> $HOME/.config/fusion-360/logs/wineprefixes.log
+  echo "$wineprefixname" >> $HOME/.config/fusion-360/logs/wineprefixes.log
 fi
 }
 
@@ -146,13 +146,78 @@ function setupact-load-winetricks {
 function setupact-load-f360exe {
   f360exe="$HOME/.config/fusion-360/bin/Fusion360installer.exe" # Search for a existing installer of Autodesk Fusion 360
   if [ -f "$f360exe" ]; then
-      echo "Autodesk Fusion 360 installer exist!"
+    echo "Autodesk Fusion 360 installer exist!"
   else
-      wget https://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Admin%20Install.exe -O Fusion360installer.exe
-      mv Fusion360installer.exe $HOME/.config/fusion-360/bin/Fusion360installer.exe
+    wget https://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Admin%20Install.exe -O Fusion360installer.exe
+    mv "Fusion360installer.exe" "$HOME/.config/fusion-360/bin/Fusion360installer.exe"
   fi
 }
 
 ###############################################################################################################################################################
 # ALL FUNCTIONS FOR DXVK AND OPENGL START HERE:                                                                                                               #
 ###############################################################################################################################################################
+
+function setupact-dxvk-opengl-1 {
+  if [ $driver_used -eq 1 ]; then
+    WINEPREFIX=$wineprefixname sh $HOME/.config/fusion-360/bin/winetricks -q dxvk
+    wget -N https://github.com/cryinkfly/Autodesk-Fusion-360-for-Linux/raw/main/files/builds/stable-branch/driver/video/dxvk/DXVK.reg
+    WINEPREFIX=$wineprefixname wine regedit.exe DXVK.reg
+  fi
+}
+
+function setupact-dxvk-opengl-2 {
+  if [ $driver_used -eq 1 ]; then
+    wget -N https://github.com/cryinkfly/Autodesk-Fusion-360-for-Linux/raw/main/files/builds/stable-branch/driver/video/dxvk/DXVK.xml
+    mv "DXVK.xml" "NMachineSpecificOptions.xml"
+  else
+    wget -N https://github.com/cryinkfly/Autodesk-Fusion-360-for-Linux/raw/main/files/builds/stable-branch/driver/video/opengl/OpenGL.xml
+    mv "OpenGL.xml" "NMachineSpecificOptions.xml"
+  fi
+}
+
+###############################################################################################################################################################
+# ALL FUNCTIONS FOR WINE AND WINETRICKS START HERE:                                                                                                           #
+###############################################################################################################################################################
+
+# Autodesk Fusion 360 will now be installed using Wine and Winetricks.
+function setupact-f360install {
+  WINEPREFIX=$wineprefixname sh winetricks -q sandbox atmlib gdiplus corefonts cjkfonts msxml4 msxml6 vcrun2017 fontsmooth=rgb winhttp win10
+  # We must install cjkfonts again then sometimes it doesn't work in the first time!
+  WINEPREFIX=$wineprefixname sh winetricks -q cjkfonts
+  setupact-dxvk-opengl-1
+  WINEPREFIX=$wineprefixname wine data/fusion360/Fusion360installer.exe -p deploy -g -f log.txt --quiet
+  WINEPREFIX=$wineprefixname wine data/fusion360/Fusion360installer.exe -p deploy -g -f log.txt --quiet
+  mkdir -p "$wineprefixname/drive_c/users/$USER/AppData/Roaming/Autodesk/Neutron Platform/Options"
+  cd "$wineprefixname/drive_c/users/$USER/AppData/Roaming/Autodesk/Neutron Platform/Options"
+  setupact-dxvk-opengl-2
+  mkdir -p "$wineprefixname/drive_c/users/$USER/Application Data/Autodesk/Neutron Platform/Options"
+  cd "$wineprefixname/drive_c/users/$USER/Application Data/Autodesk/Neutron Platform/Options"
+  setupact-dxvk-opengl-2
+  cd "$HOME/.config/fusion-360/bin"
+  setupact-f360-launcher
+  wget -N -P $HOME/.config/fusion-360/bin https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/fusion360.svg
+  setupact-log-f360-path
+  setupact-f360extensions
+  setupact-completed
+}
+
+###############################################################################################################################################################
+
+# Create a launcher for your Wineprefix of Autodesk Fusion 360.
+function setupact-f360-launcher {
+  if [ $f360_launcher -eq 1 ]; then
+    wget -N -P $HOME/.local/share/applications/wine/Programs/Autodesk https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/.desktop/Autodesk%20Fusion%20360.desktop
+    wget -N -P $HOME/.local/share/applications/wine/Programs/Autodesk https://github.com/cryinkfly/Autodesk-Fusion-360-for-Linux/blob/main/files/builds/stable-branch/.desktop/Autodesk%20Fusion%20360%20Uninstall.desktop
+    wget -N -P $HOME/.config/fusion-360/bin https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/uninstall.sh
+    chmod +x $HOME/.config/fusion-360/bin/uninstall.sh  
+    wget -N -P $HOME/.config/fusion-360/bin https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/launcher.sh
+    chmod +x $HOME/.config/fusion-360/bin/launcher.sh
+  else
+    wget -N -P $HOME/.local/share/applications/wine/Programs/Autodesk https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/.desktop/Autodesk%20Fusion%20360.desktop
+    wget -N -P $HOME/.local/share/applications/wine/Programs/Autodesk https://github.com/cryinkfly/Autodesk-Fusion-360-for-Linux/blob/main/files/builds/stable-branch/.desktop/Autodesk%20Fusion%20360%20Uninstall.desktop
+    wget -N -P $HOME/.config/fusion-360/bin https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/uninstall.sh
+    chmod +x $HOME/.config/fusion-360/bin/uninstall.sh
+    wget -P /tmp/fusion-360/logs https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/launcher.sh -O Fusion360launcher
+    setupact-f360-modify-launcher
+  fi
+}
