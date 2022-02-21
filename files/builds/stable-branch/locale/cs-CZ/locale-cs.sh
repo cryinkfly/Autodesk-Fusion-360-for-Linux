d9f3fdc08a8fd4d9749e917bd447499adba91bd5
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-02-21 15:03:48 +0100

    Update locale-cs.sh

--- files/builds/stable-branch/locale/cs-CZ/locale-cs.sh
+++ files/builds/stable-branch/locale/cs-CZ/locale-cs.sh
@@ -7,108 +7,110 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2022                                                                          #
-# Time/Date:    16:30/10.02.2022                                                                   #
-# Version:      1.4                                                                                #
+# Time/Date:    15:00/21.02.2022                                                                   #
+# Version:      1.5                                                                                #
 ####################################################################################################
 
+# Path: /$HOME/.config/fusion-360/locale/cs-CZ/locale-cs.sh
+
 ###############################################################################################################################################################
-# ALL DEFINITIONS ARE ARRANGED HERE:                                                                                                                          #
+# ALL DEFINITIONS FOR INSTALL AUTODESK FUSION 360 ARE ARRANGED HERE:                                                                                          #
 ###############################################################################################################################################################
 
 # License-checkbox
+text_license_checkbox="Přečetl jsem a přijímám licenční smlouvu."
 
-# License-checkbox
-text_license_checkbox="Přečetl jsem si podmínky a přijímám je."
+###############################################################################################################################################################
 
 # Select
 text_select="Vybrat"
 
-# Version (Standard, Flatpak or Snap)
-text_version="Verze"
-text_standard="Standard"
-text_flatpak="Flatpak (Experimentální)"
-text_snap="Snap (Experimentální)"
-
-# Driver
-text_driver="Ovladač grafiky"
-
-# Driver OpenGL
-text_driver_opengl="OpenGL (Záložní Varianta)"
-
-# Driver DXVK
-text_driver_dxvk="DXVK (Standardní Varianta)"
+###############################################################################################################################################################
 
 # Linux distribution
-text_linux_distribution="Distribuce Linuxu"
+text_linux_distribution="Linuxová distribuce"
+
+###############################################################################################################################################################
 
 # Installation location
-text_installation_location="Umístění instalace"
-
-# Installation location - Standard
-text_installation_location_standard="Standard - Nainstalujte si Autodesk Fusion 360 do své domovské složky. Například: /home/username/.wineprefixes/fusion360"
-
-# Installation location - Custom
-text_installation_location_custom="Vlastní - Nainstalujte Autodesk Fusion 360 na jiné místo. Například: /run/media/username/usb-drive/.wineprefixes/fusion360"
+text_installation_location="Řazení instalací"
 
 # Select a location with a file browser
-text_select_location_custom="Vyberte umístění pro instalaci aplikace Autodesk Fusion 360:"
+text_select_location_custom="Vyberte umístění pro instalaci Autodesk Fusion 360:"
+
+# Installation location - Standard
+text_installation_location_standard="Výchozí – Nainstalujte Autodesk Fusion 360 do své uživatelské složky. Příklad: /home/username/.wineprefixes/fusion360"
+
+# Installation location - Custom
+text_installation_location_custom="Vlastní – Nainstalujte Autodesk Fusion 360 do jiného umístění. Například: /run/media/username/usb-drive/.wineprefixes/fusion360"
+
+###############################################################################################################################################################
+
+# Driver
+text_driver="Grafický ovladač"
+
+# Driver DXVK
+text_driver_dxvk="DXVK podporuje DirectX 9, 10, ... (výchozí)"
+
+# Driver OpenGL
+text_driver_opengl="OpenGL (záložní)"
+
+###############################################################################################################################################################
+
+# Wine Version
+text_wine_version="Verze vína"
+text_wine_version_staging="Verze vína (Stáging)"
+text_wine_version_exists="Verze Wine (6.23 nebo vyšší) je již nainstalována v systému!"
+
+###############################################################################################################################################################
 
 # Checkbox - desktop-launcher-custom
-text_desktop_launcher_custom_checkbox="Změnil jsem cestu k aplikaci Autodesk Fusion 360!"
+text_desktop_launcher_custom_checkbox="Změnil jsem instalační cestu Autodesk Fusion 360!"
 
 # Question - desktop-launcher-custom
 text_desktop_launcher_custom_question="Chcete uložit změny?"
 
-# Abort the program
-text_abort="Opravdu chcete instalaci přerušit?"
-
-# Program-Exit
-text_completed_installation="Instalace aplikace Autodesk Fusion 360 je dokončena."
-
-# Program-Exit (Extensions)
-text_completed_installation_extensions="Instalace některých rozšíření pro Autodesk Fusion 360 je dokončena."
+###############################################################################################################################################################
 
 # Select a option - new_modify_deinstall
 text_select_option="Volba"
 
 # Option 1
-text_select_option_1="Nová instalace některých nebo všech součástí"
+text_select_option_1="Přeinstalace některých nebo všech součástí"
 
 # Option 2
-text_select_option_2="Aktualizujte nebo Opravte stávající instalaci"
+text_select_option_2="Aktualizujte nebo opravte stávající instalaci"
 
 # Option 3
-text_select_option_3="Nainstalujte, Opravte nebo Odinstalujte některá rozšíření"
+text_select_option_3="Nainstalujte, opravte nebo odinstalujte různá rozšíření"
 
 # Option 4
 text_select_option_4="Odinstalujte všechny součásti Autodesk Fusion 360"
 
+###############################################################################################################################################################
+
 # New installation-checkbox - new_modify_deinstall
-text_new_installation_checkbox="Nyní jsem viděl, které instalační cesty jsou k dispozici, a v dalším kroku zadám jinou cestu!"
+text_new_installation_checkbox="Nyní jsem viděl, které instalační cesty jsou k dispozici, a v dalším kroku poskytnu jinou cestu (umístění instalace)!"
 
 # Edit installation-checkbox - new_modify_deinstall
-text_edit_installation_checkbox="Poznamenal jsem si nebo zkopíroval správnou cestu z existující instalace Autodesk Fusion 360!"
+text_edit_installation_checkbox="Zapsal jsem si nebo zkopíroval do schránky správnou cestu ze stávající instalace Autodesk Fusion 360!"
 
 # Deinstall-checkbox - new_modify_deinstall
-text_deinstall_checkbox="Poznamenal jsem si nebo zkopíroval správnou cestu z existující instalace Autodesk Fusion 360 a tuto cestu jsem pak smazal zde!"
+text_deinstall_checkbox="Zapsal jsem si správnou cestu ze stávající instalace aplikace Autodesk Fusion 360 nebo ji umístil do schránky a poté zde tuto cestu smazal!"
 
 # Question - Deinstall a exist Autodesk Fusion 360 installation
-text_deinstall_question="Chcete uložit změny a odstranit správnou stávající instalaci aplikace Autodesk Fusion 360?"
+text_deinstall_question="Opravdu chcete uložit změny a odstranit stávající instalaci Autodesk Fusion 360?"
 
 # Select a location with a file browser - new_modify_deinstall
-text_select_location_deinstall="Vyberte existující instalaci aplikace Autodesk Fusion 360:"
+text_select_location_deinstall="Vyberte svou stávající instalaci Autodesk Fusion 360:"
 
 # Program-Exit - new_modify_deinstall
-text_completed_deinstallation="Odinstalování aplikace Autodesk Fusion 360 je dokončeno."
+text_completed_deinstallation="Odinstalace aplikace Autodesk Fusion 360 je dokončena."
 
-# Select CZECH-Plugin files
-text_select_czech_plugin="Vyberte instalační soubor:"
-
-# No Plugin was not found
-text_info_czech_plugin="Žádný soubor nevybrán."
+###############################################################################################################################################################
 
 # Extension
-text_extension="Rozšíření"
+text_extension="Erweiterung"
 
 # Extension Description
 text_extension_description="Popis"
@@ -123,8 +125,50 @@ text_extension_description_7="Umožňuje uživateli importovat/aktualizovat para
 text_extension_description_8="Toto rozšíření umožňuje programovat více než 50 různých výrobců robotů a 500 robotů přímo z Autodesk Fusion 360."
 text_extension_description_9="Tento plug-in je konektorem mezi Autodesk® Fusion 360™ a webem Ultimaker Digital Factory a jeho službami."
 
+# Select CZECH-Plugin files
+text_select_czech_plugin="Vyberte instalační soubor:"
+
+# No Plugin was not found
+text_info_czech_plugin="Žádný soubor nevybrán."
+
+# Program-Exit (Extensions)
+text_completed_installation_extensions="Instalace některých rozšíření pro Autodesk Fusion 360 je dokončena."
+
+###############################################################################################################################################################
+
+# Program-Exit
+text_completed_installation="Instalace Autodesk Fusion 360 je dokončena."
+
+# Abort the program
+text_abort="Opravdu chcete zrušit instalaci?"
+
 # Error
 text_error="Vyskytla se neočekávaná chyba!"
 
+###############################################################################################################################################################
+# ALL DEFINITIONS FOR UNINSTALL AUTODESK FUSION 360 ARE ARRANGED HERE:                                                                                        #
+###############################################################################################################################################################
 
+text_uninstall_question="Opravdu chcete odinstalovat Autodesk Fusion 360 ze svého systému?"
+text_uninstall_cancel="Odinstalace byla přerušena!"
 
+text_uninstall_checkbox="Zapsal jsem/zkopíroval jsem správnou cestu ze stávající instalace Autodesk Fusion 360 a poté jsem cestu v tomto zobrazení zde smazal!"
+text_uninstall_edit_question="Chcete uložit změny a odinstalovat software?"
+
+text_uninstall_path="Vyberte adresář Wineprefix vaší instalace Autodesk Fusion 360, kterou chcete odinstalovat! Například: /home/user/.wineprefixes/fusion360"
+text_uninstall_path_select="Vyberte adresář Wineprefix..."
+
+text_uninstall_completed="Odinstalace aplikace Autodesk Fusion 360 je dokončena."
+
+###############################################################################################################################################################
+# ALL DEFINITIONS FOR UPDATE AUTODESK FUSION 360 ARE ARRANGED HERE:                                                                                        #
+###############################################################################################################################################################
+
+text_no_update_info="Nebyla nalezena žádná novější verze, takže váš Autodesk fusion 360 je aktuální!"
+
+text_skip_update_info="Aktualizace byla přeskočena! Aktualizujte prosím svou verzi Autodesk Fusion 360 brzy!"
+text_skip_update_question="Opravdu chcete přeskočit hledání aktualizace Autodesk Fusion 360?"
+
+text_update_question="Byla vydána nová verze! Chcete provést aktualizaci nyní?"
+
+text_no_connection_warning="Spojení se serverem se nepodařilo navázat! Kontrola nových aktualizací byla přeskočena! Zkontrolujte prosím své internetové připojení!"
