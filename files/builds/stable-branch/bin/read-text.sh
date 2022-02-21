926e525ad39cc8a0a8914d6ad829caeb091f7d39
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-02-21 16:36:18 +0100

    Update and rename files/builds/stable-branch/locale/profile-locale.sh to files/builds/stable-branch/bin/read-text.sh

--- files/builds/stable-branch/bin/read-text.sh
+++ files/builds/stable-branch/bin/read-text.sh
@@ -2,22 +2,22 @@
 
 ####################################################################################################
 # Name:         Autodesk Fusion 360 - Setup Wizard (Linux)                                         #
-# Description:  With this file the language will be loaded automatically!                          #
+# Description:  With this file we can read text files!                          #
 # Author:       Steve Zabka                                                                        #
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2022                                                                          #
-# Time/Date:    10:30/21.02.2022                                                                   #
-# Version:      0.1                                                                                #
+# Time/Date:    16:30/21.02.2022                                                                   #
+# Version:      0.2                                                                                #
 ####################################################################################################
 
-# Path: /$HOME/.config/fusion-360/locale/profile-locale.sh
+# Path: /$HOME/.config/fusion-360/bin/read-text.sh
 
 ###############################################################################################################################################################
 # ALL FUNCTIONS ARE ARRANGED HERE:                                                                                                                            #
 ###############################################################################################################################################################
 
-# Read a specific line from a File ($HOME/.config/fusion-360/logs/profile-locale.log)
+# Read a specific line from a text file!
 FILE="$1"
 LINE_NO=$2
 i=0
