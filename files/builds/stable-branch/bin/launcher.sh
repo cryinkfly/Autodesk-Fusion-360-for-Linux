dda7f143a15b99ade12fb85a6b89f1618f0f6f9c
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-03-10 12:22:07 +0100

    Solved a bug and change the functions!

--- files/builds/stable-branch/bin/launcher.sh
+++ files/builds/stable-branch/bin/launcher.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                       #
 # License:      MIT                                                         #
 # Copyright (c) 2020-2022                                                   #
-# Time/Date:    11:30/10.03.2022                                            #
-# Version:      1.5                                                         #
+# Time/Date:    12:30/10.03.2022                                            #
+# Version:      1.6                                                         #
 #############################################################################
 
 # Path: /$HOME/.config/fusion-360/bin/launcher.sh
@@ -24,11 +24,13 @@
 # This feature will check if there is a new version of Autodesk Fusion 360.
 function setupact-check-fusion360 {
   wget -N -P $HOME/.config/fusion-360/bin https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/build-version.txt  
-  online_build_version=`. $HOME/.config/fusion-360/bin/read-text.sh $HOME/.config/fusion-360/bin/build-version.txt 1`
+  online_build_version=`cat $HOME/.config/fusion-360/bin/build-version.txt`
+  echo "Online Build-Version: $online_build_version"
 }
 
 function setupact-config-update {
-  system_build_version=`. $HOME/.config/fusion-360/bin/read-text.sh $HOME/.wineprefixes/fusion360/drive_c/users/$USER/AppData/Roaming/Autodesk/Autodesk\ Fusion\ 360/API/version.txt 1`
+  system_build_version=`cat $HOME/.wineprefixes/fusion360/drive_c/users/$USER/AppData/Roaming/Autodesk/Autodesk\ Fusion\ 360/API/version.txt`
+  echo "System Build-Version: $system_build_version"
   if [ "$online_build_version" = "$system_build_version" ]; then
     echo "Do nothing!"
     get_update=0
