726d8c779f51ca451315e497ce54663a678017bf
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-03-10 11:38:21 +0100

    Change the function for check the version!
    The function now reads the version number of Autodesk Fusion 360 on your system and compares it with the version number from my database.
    
    Please delete the old file: update-config.txt !!!
    
    Here is the path: $HOME/.config/fusion-360/bin/update-config.txt

--- files/builds/stable-branch/bin/launcher.sh
+++ files/builds/stable-branch/bin/launcher.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                       #
 # License:      MIT                                                         #
 # Copyright (c) 2020-2022                                                   #
-# Time/Date:    12:00/21.02.2022                                            #
-# Version:      1.4                                                         #
+# Time/Date:    11:30/10.03.2022                                            #
+# Version:      1.5                                                         #
 #############################################################################
 
 # Path: /$HOME/.config/fusion-360/bin/launcher.sh
@@ -23,17 +23,17 @@
 
 # This feature will check if there is a new version of Autodesk Fusion 360.
 function setupact-check-fusion360 {
-  wget -N -P $HOME/.config/fusion-360/bin https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/update-config.txt  
+  wget -N -P $HOME/.config/fusion-360/bin https://raw.githubusercontent.com/cryinkfly/Autodesk-Fusion-360-for-Linux/main/files/builds/stable-branch/bin/build-version.txt  
 }
 
 function setupact-config-update {
-  config_update=`. $HOME/.config/fusion-360/bin/read-text.sh $HOME/.config/fusion-360/bin/update-config.txt 1`
-  if [ "$config_update" = "Update=1" ]; then
+  config_update=`. $HOME/.config/fusion-360/bin/read-text.sh $HOME/.wineprefixes/fusion360/drive_c/users/$USER/AppData/Roaming/Autodesk/Autodesk Fusion 360/API/version.txt 1`
+  if [ "$config_update" = "$config_update" ]; then
+        echo "Do nothing!"
+    get_update=0
+  else
     # A value of 0 means that there is no update and a value of 1 will notify the user that there is an update.
     get_update=1
-  else
-    echo "Do nothing!"
-    get_update=0
   fi 
 }
 
