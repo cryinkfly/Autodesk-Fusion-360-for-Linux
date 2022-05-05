8c43b996647c560ee389aab674ca9b73a9d084d5
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-05-05 18:53:53 +0200

    Update install.sh

--- files/builds/development-branch/install.sh
+++ files/builds/development-branch/install.sh
@@ -7,7 +7,7 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2022                                                                          #
-# Time/Date:    18:45/05.05.2022                                                                   #
+# Time/Date:    19:00/05.05.2022                                                                   #
 # Version:      1.7.9 -> 1.8.0                                                                     #
 ####################################################################################################
 
@@ -112,14 +112,14 @@ function SP_SERVER_LIST {
   SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 76'`
   
   # EXTENSIONS:
-  SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 83'`
-  SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 84'`
-  SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 85'`
-  SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 86'`
-  SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 87'`
-  SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 88'`
-  SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 89'`
-  SP_SERVER_30=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 90'`
+  SP_SERVER_31=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 83'`
+  SP_SERVER_32=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 84'`
+  SP_SERVER_33=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 85'`
+  SP_SERVER_34=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 86'`
+  SP_SERVER_35=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 87'`
+  SP_SERVER_36=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 88'`
+  SP_SERVER_37=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 89'`
+  SP_SERVER_38=`cat $SP_PATH/servers/server-list.txt | awk 'NR == 90'`
 }
 
 ###############################################################################################################################################################
@@ -290,4 +290,283 @@ function SP_DXVK_OPENGL_2 {
     wget -N $SP_SERVER_24
     mv "OpenGL.xml" "NMachineSpecificOptions.xml"
   fi
+}
+
+###############################################################################################################################################################
+# ALL FUNCTIONS FOR WINE AND WINETRICKS START HERE:                                                                                                           #
+###############################################################################################################################################################
+
+# Autodesk Fusion 360 will now be installed using Wine and Winetricks.
+function SP_FUSION360_INSTALL {
+  # Note that the winetricks sandbox verb merely removes the desktop integration and Z: drive symlinks and is not a "true" sandbox.
+  # It protects against errors rather than malice. It's useful for, e.g., keeping games from saving their settings in random subdirectories of your home directory. 
+  # But it still ensures that wine, for example, no longer has access permissions to Home! 
+  # For this reason, the EXE files must be located directly in the Wineprefix folder!
+  WINEPREFIX=$WP_PATH sh $SP_PATH/bin/winetricks -q sandbox
+  # We must install some packages!
+  WINEPREFIX=$WP_PATH sh $SP_PATH/bin/winetricks -q atmlib gdiplus corefonts cjkfonts msxml4 msxml6 vcrun2017 fontsmooth=rgb winhttp win10
+  # We must install cjkfonts again then sometimes it doesn't work in the first time!
+  WINEPREFIX=$WP_PATH sh $SP_PATH/bin/winetricks -q cjkfonts
+  SP_DXVK_OPENGL_1
+  # We must copy the EXE-file directly in the Wineprefix folder (Sandbox-Mode)!
+  cp "$SP_PATH/downloads/Fusion360installer.exe" "$wineprefixname/drive_c/users/$USER/Downloads"
+  WINEPREFIX=$WP_PATH wine $wineprefixname/drive_c/users/$USER/Downloads/Fusion360installer.exe -p deploy -g -f log.txt --quiet
+  WINEPREFIX=$WP_PATH wine $wineprefixname/drive_c/users/$USER/Downloads/Fusion360installer.exe -p deploy -g -f log.txt --quiet
+  mkdir -p "$WP_PATH/drive_c/users/$USER/AppData/Roaming/Autodesk/Neutron Platform/Options"
+  cd "$WP_PATH/drive_c/users/$USER/AppData/Roaming/Autodesk/Neutron Platform/Options"
+  SP_DXVK_OPENGL_2
+  mkdir -p "$WP_PATH/drive_c/users/$USER/AppData/Local/Autodesk/Neutron Platform/Options"
+  cd "$WP_PATH/drive_c/users/$USER/AppData/Local/Autodesk/Neutron Platform/Options"
+  SP_DXVK_OPENGL_2
+  mkdir -p "$WP_PATH/drive_c/users/$USER/Application Data/Autodesk/Neutron Platform/Options"
+  cd "$WP_PATH/drive_c/users/$USER/Application Data/Autodesk/Neutron Platform/Options"
+  SP_DXVK_OPENGL_2
+  cd "SP_PATH/bin"
+  SP_FUSION360_SHORTCUTS_LOAD
+  SP_FUSION360_EXTENSIONS
+  SP_COMPLETED
+}
+
+###############################################################################################################################################################
+# ALL FUNCTIONS FOR SUPPORTED LINUX DISTRIBUTIONS START HERE:                                                                                                 #
+###############################################################################################################################################################
+
+function OS_ARCHLINUX {
+  echo "Checking for multilib..."
+  if ARCHLINUX_VERIFY_MULTILIB ; then
+    echo "multilib found. Continuing..."
+    sudo pacman -Sy --needed wine wine-mono wine_gecko winetricks p7zip curl cabextract samba ppp
+    SP_FUSION360_INSTALL
+  else
+    echo "Enabling multilib..."
+    echo "[multilib]" | sudo tee -a /etc/pacman.conf
+    echo "Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
+    sudo pacman -Sy --needed wine wine-mono wine_gecko winetricks p7zip curl cabextract samba ppp
+    SP_FUSION360_INSTALL
+  fi
+}
+
+function ARCHLINUX_VERIFY_MULTILIB {
+  if cat /etc/pacman.conf | grep -q '^\[multilib\]$' ; then
+    true
+  else
+    false
+  fi
+}
+
+###############################################################################################################################################################
+
+function DEBIAN_BASED_1 {
+  # Some systems require this command for all repositories to work properly and for the packages to be downloaded for installation!
+  sudo apt-get --allow-releaseinfo-change update  
+  # Added i386 support for wine!
+  sudo dpkg --add-architecture i386
+}
+
+function DEBIAN_BASED_2 {
+  sudo apt-get update
+  sudo apt-get install p7zip p7zip-full p7zip-rar curl winbind cabextract wget
+  sudo apt-get install --install-recommends winehq-staging
+  SP_FUSION360_INSTALL
+}
+
+function OS_DEBIAN_10 {
+  sudo apt-add-repository -r 'deb https://dl.winehq.org/wine-builds/debian/ buster main'
+  wget -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10//Release.key -O Release.key -O- | sudo apt-key add -
+  sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/ ./'
+}
+
+function OS_DEBIAN_11 {
+  sudo apt-add-repository -r 'deb https://dl.winehq.org/wine-builds/debian/ bullseye main'
+  wget -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_11//Release.key -O Release.key -O- | sudo apt-key add -
+  sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_11/ ./'
+}
+
+function OS_UBUNTU_18 {
+  sudo apt-add-repository -r 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+  wget -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key -O Release.key -O- | sudo apt-key add -
+  sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./'
+}
+
+function OS_UBUNTU_20 {
+  sudo add-apt-repository -r 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+  wget -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_20.04/Release.key -O Release.key -O- | sudo apt-key add -
+  sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_20.04/ ./'
+}
+
+function OS_UBUNTU_22 {
+  sudo add-apt-repository -r 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+  wget -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_22.04/Release.key -O Release.key -O- | sudo apt-key add -
+  sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_22.04/ ./'
+}
+
+###############################################################################################################################################################
+
+function FEDORA_BASED_1 {
+  sudo dnf update
+  sudo dnf upgrade
+  sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+}
+
+function FEDORA_BASED_2 {
+  sudo dnf install p7zip p7zip-plugins curl wget wine cabextract
+  SP_FUSION360_INSTALL
+}
+
+function OS_FEDORA_34 {
+  sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/Emulators:/Wine:/Fedora/Fedora_34/Emulators:Wine:Fedora.repo
+}
+
+function OS_FEDORA_35 {
+  sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/Emulators:/Wine:/Fedora/Fedora_35/Emulators:Wine:Fedora.repo
+}
+
+function OS_FEDORA_36 {
+  sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/Emulators:/Wine:/Fedora/Fedora_36/Emulators:Wine:Fedora.repo
+}
+
+###############################################################################################################################################################
+
+function OS_OPENSUSE_152 {
+  su -c 'zypper up && zypper rr https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.2/ wine && zypper install p7zip-full curl wget wine cabextract'
+  SP_FUSION360_INSTALL
+}
+
+function OS_OPENSUSE_153 {
+  su -c 'zypper up && zypper rr https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.3/ wine && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.3/ wine && zypper install p7zip-full curl wget wine cabextract'
+  SP_FUSION360_INSTALL
+}
+
+# Has not been published yet!
+function OS_OPENSUSE_154 {
+  su -c 'zypper up && zypper rr https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.4/ wine && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Leap_15.4/ wine && zypper install p7zip-full curl wget wine cabextract'
+  SP_FUSION360_INSTALL
+}
+
+function OS_OPENSUSE_TW {
+  su -c 'zypper up && zypper rr https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Tumbleweed/ wine && zypper ar -cfp 95 https://download.opensuse.org/repositories/Emulators:/Wine/openSUSE_Tumbleweed/ wine && zypper install p7zip-full curl wget wine cabextract'
+  SP_FUSION360_INSTALL
+}
+
+###############################################################################################################################################################
+
+function OS_REDHAT_LINUX_8 {
+  sudo subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
+  sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+  sudo dnf upgrade
+  sudo dnf install wine
+  SP_FUSION360_INSTALL
+}
+
+function OS_REDHAT_LINUX_9 {
+  sudo subscription-manager repos --enable codeready-builder-for-rhel-9-x86_64-rpms
+  sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+  sudo dnf upgrade
+  sudo dnf install wine
+  SP_FUSION360_INSTALL
+}
+
+###############################################################################################################################################################
+
+function OS_SOLUS_LINUX {
+  sudo eopkg install -y wine winetricks p7zip curl cabextract samba ppp
+  SP_FUSION360_INSTALL
+}
+
+###############################################################################################################################################################
+
+function OS_VOID_LINUX {
+  sudo xbps-install -Sy wine wine-mono wine-gecko winetricks p7zip curl cabextract samba ppp
+  SP_FUSION360_INSTALL
+}
+
+###############################################################################################################################################################
+
+function OS_GENTOO_LINUX {
+  sudo emerge -nav virtual/wine app-emulation/winetricks app-emulation/wine-mono app-emulation/wine-gecko app-arch/p7zip app-arch/cabextract net-misc/curl net-fs/samba net-dialup/ppp
+  SP_FUSION360_INSTALL
+}
+
+###############################################################################################################################################################
+# ALL FUNCTIONS FOR THE EXTENSIONS START HERE:                                                                                                                #
+###############################################################################################################################################################
+
+# Install a extension: Airfoil Tools
+
+function EXTENSION_AIRFOIL_TOOLS {
+  cd "$WP_PATH/drive_c/users/$USER/Downloads"
+  wget -N $SP_SERVER_31 &&
+  WINEPREFIX=$WP_PATH wine AirfoilTools_win64.msi
+}
+
+###############################################################################################################################################################
+
+# Install a extension: Additive Assistant (FFF)
+
+function EXTENSION_ADDITIVE_ASSISTANT {
+  cd "$WP_PATH/drive_c/users/$USER/Downloads"
+  wget -N $SP_SERVER_32 &&
+  WINEPREFIX=$WP_PATH msiexec /i AdditiveAssistant.bundle-win64.msi
+}
+
+###############################################################################################################################################################
+
+# Install a extension: Czech localization for F360
+function EXTENSION_CZECH_LOCALE {
+  SP_SEARCH_EXTENSION_CZECH_LOCALE
+  WINEPREFIX=$WP_PATH msiexec /i $CZECH_LOCALE_EXTENSION
+}
+
+###############################################################################################################################################################
+
+# Install a extension: HP 3D Printers for Autodesk® Fusion 360™
+function EXTENSION_HP_3DPRINTER_CONNECTOR {
+  cd "$WP_PATH/drive_c/users/$USER/Downloads"
+  wget -N $SP_SERVER_33 &&
+  WINEPREFIX=$WP_PATH msiexec /i HP_3DPrinters_for_Fusion360-win64.msi
+}
+
+###############################################################################################################################################################
+
+# Install a extension: Helical Gear Generator
+function EXTENSION_HELICAL_GEAR_GENERATOR {
+  cd "$WP_PATH/drive_c/users/$USER/Downloads"
+  wget -N $SP_SERVER_34 &&
+  WINEPREFIX=$WP_PATH msiexec /i HelicalGear_win64.msi
+}
+
+###############################################################################################################################################################
+
+# Install a extension: OctoPrint for Autodesk® Fusion 360™
+function EXTENSION_OCTOPRINT {
+  cd "$WP_PATH/drive_c/users/$USER/Downloads"
+  wget -N $SP_SERVER_35 &&
+  WINEPREFIX=$WP_PATH msiexec /i OctoPrint_for_Fusion360-win64.msi
+}
+
+###############################################################################################################################################################
+
+# Install a extension: Parameter I/O
+function EXTENSION_PARAMETER_IO {
+  cd "$WP_PATH/drive_c/users/$USER/Downloads"
+  wget -N $SP_SERVER_36 &&
+  WINEPREFIX=$WP_PATH msiexec /i ParameterIO_win64.msi
+}
+
+###############################################################################################################################################################
+
+# Install a extension: RoboDK
+function EXTENSION_ROBODK {
+  cd "$WP_PATH/drive_c/users/$USER/Downloads"
+  wget -N $SP_SERVER_37 &&
+  WINEPREFIX=$WP_PATH msiexec /i RoboDK.bundle-win64.msi
+}
+
+###############################################################################################################################################################
+
+# Install a extension: Ultimaker Digital Factory for Autodesk Fusion 360™
+function EXTENSION_ULTIMAKER_DIGITAL_FACTORY {
+  cd "$WP_PATH/drive_c/users/$USER/Downloads"
+  wget -N SP_SERVER_38 &&
+  WINEPREFIX=$WP_PATH msiexec /i Ultimaker_Digital_Factory-win64.msi
 }
