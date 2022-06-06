b0ea6a3f0ca165e05d9cfc2ffc0e9a88edace9cf
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-06-06 21:07:28 +0200

    Update locale-cz.sh

--- files/builds/development-branch/locale-cz.sh
+++ files/builds/development-branch/locale-cz.sh
@@ -2,127 +2,125 @@
 
 ####################################################################################################
 # Name:         Autodesk Fusion 360 - Setup Wizard (Linux)                                         #
-# Description:  This is the English translation for the Setup Wizard.                              #
+# Description:  This is the Czech translation for the Setup Wizard.                                #
 # Author:       Steve Zabka                                                                        #
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2022                                                                          #
-# Time/Date:    20:20/06.06.2022                                                                   #
+# Time/Date:    21:10/06.06.2022                                                                   #
 # Version:      1.5 -> 1.6                                                                         #
 ####################################################################################################
 
-# Path: /$HOME/.fusion360/locale/en-EN/locale-en.sh
+# Path: /$HOME/.fusion360/locale/cs-CZ/locale-cs.sh
 
 ###############################################################################################################################################################
 # ALL DEFINITIONS FOR AUTODESK FUSION 360 ARE ARRANGED HERE:                                                                                                  #
 ###############################################################################################################################################################
 
 # Window Title (Setup Wizard)
-program_name="Autodesk Fusion 360 for Linux - Setup Wizard"
+SP_TITLE="Průvodce instalací - Autodesk Fusion 360 pro Linux"
 
 # Window Title:
-SP_TITLE="Welcome to the Autodesk Fusion 360 Installer for Linux"
+SP_SUBTITLE="Vítejte v instalačním programu Autodesk Fusion 360 pro Linux"
 
 # Welcome Screen:
-SP_WELCOME_LABEL_1="This setup wizard installs Autodesk Fusion 360 on your computer so that you can also work on your projects on Linux."
-SP_WELCOME_LABEL_2="Click Ok to continue or Cancel to exit the setup wizard."
-SP_WELCOME_TOOLTIP_1="Here you get more informations about this setup wizard."
-SP_WELCOME_TOOLTIP_2="Here you can adjust the default setting. For example the language."
+SP_WELCOME_LABEL_1="Tento průvodce nastavením nainstaluje Autodesk Fusion 360 do vašeho počítače, abyste mohli na svých projektech pracovat také v systému Linux."
+SP_WELCOME_LABEL_2="Klikněte na OK pro pokračování nebo Storno pro ukončení průvodce nastavením."
+SP_WELCOME_TOOLTIP_1="Zde získáte další informace o tomto průvodci nastavením."
+SP_WELCOME_TOOLTIP_2="Zde můžete upravit výchozí nastavení. Například jazyk."
 
 ###############################################################################################################################################################
 
 # General Settings:
-SP_SETTINGS_TITLE="General Settings"
-SP_SETTINGS_LABEL_1="Here you have the option to adjust* further settings:"
-SP_SETTINGS_LABEL_2="*Please remember that any change will affect the Autodesk Fusion 360 installation!"
-SP_LOCALE_LABEL="Languages"
+SP_SETTINGS_TITLE="Obecné nastavení"
+SP_SETTINGS_LABEL_1="Zde máte možnost upravit* další nastavení:"
+SP_SETTINGS_LABEL_2="*Pamatujte prosím, že jakákoli změna ovlivní instalaci Autodesk Fusion 360!"
+SP_LOCALE_LABEL="Jazyky"
 SP_LOCALE_SELECT=$(echo "Czech,English,German,Spanish,French,Italian,Japanese,Korean,Chinese")
-WP_DRIVER_LABEL="Graphics Driver"
+WP_DRIVER_LABEL="Ovladač grafiky"
 WP_DRIVER_SELECT=$(echo "DXVK,OpenGL")
 
-# Locale value:
-SP_LOCALE="EN"
-
 # License Checkbox:
-SP_LICENSE_CHECK_LABEL="I have read the terms and conditions and I accept them."
+SP_LICENSE_CHECK_LABEL="Přečetl jsem si podmínky a souhlasím s nimi."
 
 ###############################################################################################################################################################
 
 # Wineprefix Info - Autodesk Fusion 360 exist on the computer:
-SP_LOGFILE_WINEPREFIX_INFO_TITLE="Welcome to the Autodesk Fusion 360 Installer for Linux"
-SP_LOGFILE_WINEPREFIX_INFO_LABEL_1="A previous installation of Autodesk Fusion 360 has been detected on your system!"
-SP_LOGFILE_WINEPREFIX_INFO_LABEL_2="Therefore, please select one of the options below to continue!"
-SP_LOGFILE_WINEPREFIX_INFO_TOOLTIP_1="Create a new Wineprefix in a different location!"
-SP_LOGFILE_WINEPREFIX_INFO_TOOLTIP_2="Repair the current Wineprefix on your system!"
-SP_LOGFILE_WINEPREFIX_INFO_TOOLTIP_3="Remove the current Wineprefix from your system!"
+SP_LOGFILE_WINEPREFIX_INFO_TITLE="$SP_SUBTITLE"
+SP_LOGFILE_WINEPREFIX_INFO_LABEL_1="Ve vašem systému byla zjištěna předchozí instalace Autodesk Fusion 360!"
+SP_LOGFILE_WINEPREFIX_INFO_LABEL_2="Proto pro pokračování vyberte jednu z níže uvedených možností!"
+SP_LOGFILE_WINEPREFIX_INFO_TOOLTIP_1="Vytvořte nový Wineprefix na jiném místě!"
+SP_LOGFILE_WINEPREFIX_INFO_TOOLTIP_2="Opravte aktuální Wineprefix na vašem systému!"
+SP_LOGFILE_WINEPREFIX_INFO_TOOLTIP_3="Odstraňte aktuální Wineprefix ze svého systému!"
 
 ###############################################################################################################################################################
 
 # Linux distribution - Configuration:
-SP_OS_TITLE="Linux distribution - Configuration"
-SP_OS_LABEL_1="In this step you can now select your Linux distribution to install the required packages for the installation."
-SP_OS_LABEL_2="Linux distribution:"
+SP_OS_TITLE="Linux distribuce - Konfigurace"
+SP_OS_LABEL_1="V tomto kroku nyní můžete vybrat svou distribuci Linuxu a nainstalovat potřebné balíčky pro instalaci."
+SP_OS_LABEL_2="Linux distribuce:"
 SP_OS_SELECT=$(echo "Arch Linux,Debian 10,Debian 11,EndeavourOS,Fedora 35,Fedora 36,Linux Mint 19.x,Linux Mint 20.x,Manjaro Linux,openSUSE Leap 15.3,openSUSE Leap 15.4,openSUSE Tumbleweed,Red Hat Enterprise Linux 8.x,Red Hat Enterprise Linux 9.x,Solus,Ubuntu 18.04,Ubuntu 20.04,Ubuntu 22.04,Void Linux,Gentoo Linux")
 
 ###############################################################################################################################################################
 
 # Installation Folder - Configuration:
-SP_INSTALLDIR_TITLE="Select the destination of the installation"
-SP_INSTALLDIR_LABEL_1="The Setup Wizard will install Autodesk Fusion 360 to the following directory*."
-SP_INSTALLDIR_LABEL_2="Directory:"
-SP_INSTALLDIR_LABEL_3="*You can also choose a different directory for the installation by clicking in the field."
+SP_INSTALLDIR_TITLE="Vyberte cíl instalace"
+SP_INSTALLDIR_LABEL_1="Průvodce nastavením nainstaluje Autodesk Fusion 360 do následujícího adresáře*."
+SP_INSTALLDIR_LABEL_2="Adresář:"
+SP_INSTALLDIR_LABEL_3="*Kliknutím do pole můžete také vybrat jiný adresář pro instalaci."
 
 ###############################################################################################################################################################
 
 # Directory info:
-SP_INSTALLDIR_INFO_TITLE="Select Installation Directory - Info"
-SP_INSTALLDIR_INFO_LABEL_1="Danger! This directory already exists!"
-SP_INSTALLDIR_INFO_LABEL_2="Please select a different directory."
+SP_INSTALLDIR_INFO_TITLE="Vyberte Instalační adresář - Informace"
+SP_INSTALLDIR_INFO_LABEL_1="Nebezpečí! Tento adresář již existuje!"
+SP_INSTALLDIR_INFO_LABEL_2="Vyberte prosím jiný adresář."
 
 ###############################################################################################################################################################
 
 # Wine Version
-SP_WINE_SETTINGS_TITLE="Select Wine Version"
-SP_WINE_SETTINGS_LABEL_1="Here you have to decide between two options*."
-SP_WINE_SETTINGS_LABEL_2="Select:"
-SP_WINE_VERSION_SELECT=$(echo "Wine Version (Staging),Wine version (6.23 or higher) is already installed on the system!")
-SP_WINE_SETTINGS_LABEL_3="*Depending on which option is selected, further packages will be installed on your system!"
+SP_WINE_SETTINGS_TITLE="Vyberte verzi vína"
+SP_WINE_SETTINGS_LABEL_1="Zde se musíte rozhodnout mezi dvěma možnostmi*."
+SP_WINE_SETTINGS_LABEL_2="Vybrat:"
+SP_WINE_VERSION_SELECT=$(echo "Verze vína (Staging), Verze vína (6.23 nebo vyšší) je již nainstalována v systému!")
+SP_WINE_SETTINGS_LABEL_3="*V závislosti na zvolené možnosti se na váš systém nainstalují další balíčky!"
 
 ###############################################################################################################################################################
 
 # Extension - Configuration:
-SP_EXTENSION_SELECT="Select"
-SP_EXTENSION_NAME="Extension"
-SP_EXTENSION_DESCRIPTION="Description"
+SP_EXTENSION_SELECT="Vybrat"
+SP_EXTENSION_NAME="Rozšíření"
+SP_EXTENSION_DESCRIPTION="Popis"
 
-SP_EXTENSION_LIST="$SP_PATH/locale/en-US/extensions-en.txt"
+SP_EXTENSION_LIST="$SP_PATH/locale/cs-CZ/extensions-cs.txt"
 
-SP_SEARCH_EXTENSION_CZECH_LOCALE_TITLE="Install the Czech-Locale-Extension"
-SP_SEARCH_EXTENSION_CZECH_LOCALE_LABEL_1="Select the file* in which the extension is located!"
-SP_SEARCH_EXTENSION_CZECH_LOCALE_LABEL_2="File:"
-SP_SEARCH_EXTENSION_CZECH_LOCALE_LABEL_3="By default you will find your downloaded extensions in the download directory."
+SP_SEARCH_EXTENSION_CZECH_LOCALE_TITLE="Nainstalujte rozšíření Czech-Locale-Extension"
+SP_SEARCH_EXTENSION_CZECH_LOCALE_LABEL_1="Vyberte soubor*, ve kterém je přípona umístěna!"
+SP_SEARCH_EXTENSION_CZECH_LOCALE_LABEL_2="Soubor:"
+SP_SEARCH_EXTENSION_CZECH_LOCALE_LABEL_3="Ve výchozím nastavení najdete stažená rozšíření v adresáři pro stahování."
 
 ###############################################################################################################################################################
 # ALL DEFINITIONS FOR UNINSTALL AUTODESK FUSION 360 ARE ARRANGED HERE:                                                                                        #
 ###############################################################################################################################################################
 
-DL_TITLE="Welcome to the Autodesk Fusion 360 Uninstaller for Linux"
-DL_WELCOME_LABEL_1="Autodesk Fusion 360 will be uninstalled from your computer!"
-DL_WELCOME_LABEL_2="Click Ok to continue or Cancel to exit this Uninstaller."
-DL_WELCOME_TOOLTIP_1="Here you get more informations about this Uninstaller."
+DL_TITLE="Vítejte v odinstalačním programu Autodesk Fusion 360 pro Linux"
+DL_SUBTITLE="Vítejte v odinstalačním programu Autodesk Fusion 360 pro Linux"
+DL_WELCOME_LABEL_1="Autodesk Fusion 360 bude z vašeho počítače odinstalován!"
+DL_WELCOME_LABEL_2="Klikněte na OK pro pokračování nebo Storno pro ukončení tohoto odinstalačního programu."
+DL_WELCOME_TOOLTIP_1="Zde získáte další informace o tomto odinstalačním programu."
 
-DL_WINEPREFIXES_DEL_INFO_TEXT="Are you sure you want to delete the selected Wineprefix from your computer?"
-DL_WINEPREFIXES_DEL_INFO_LABEL="Yes, I am aware that all my personal data will be lost in the Wineprefix."
-
-###############################################################################################################################################################
-
-SP_COMPLETED_TEXT="Autodesk Fusion 360 was successfully installed and setup on your computer."
-SP_COMPLETED_CHECK_LABEL="Run Autodesk Fusion 360"
+DL_WINEPREFIXES_DEL_INFO_TEXT="Opravdu chcete smazat vybraný Wineprefix z vašeho počítače?"
+DL_WINEPREFIXES_DEL_INFO_LABEL="Ano, jsem si vědom toho, že všechna moje osobní data budou ve Wineprefixu ztracena."
 
 ###############################################################################################################################################################
+
+SP_COMPLETED_TEXT="Autodesk Fusion 360 byl úspěšně nainstalován a nastaven na vašem počítači."
+SP_COMPLETED_CHECK_LABEL="Spusťte Autodesk Fusion 360"
+
+###############################################################################################################################################################
 ###############################################################################################################################################################
 
-SELECT="Select"
-WINEPREFIXES_TYPE="Wineprefixes Type"
-WINEPREFIXES_DRIVER="Wineprefixes Driver"
-WINEPREFIXES_DIRECTORY="Wineprefixes Directory"
+SELECT="Vybrat"
+WINEPREFIXES_TYPE="Typ předpony vína"
+WINEPREFIXES_DRIVER="Ovladač pro předpony vína"
+WINEPREFIXES_DIRECTORY="Adresář předpon vína"
