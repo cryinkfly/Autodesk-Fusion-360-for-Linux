9ce997f636a14dfcab3c51b54866bedaff0741cb
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2022-06-08 21:27:18 +0200

    Update and rename files/builds/development-branch/install.sh to files/builds/stable-branch/install.sh

--- files/builds/stable-branch/install.sh
+++ files/builds/stable-branch/install.sh
@@ -7,8 +7,8 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2022                                                                          #
-# Time/Date:    20:50/08.06.2022                                                                   #
-# Version:      1.7.9 -> 1.8.0                                                                     #
+# Time/Date:    21:30/08.06.2022                                                                   #
+# Version:      1.8.0                                                                              #
 ####################################################################################################
 
 # Path: /$HOME/.fusion360/bin/install.sh
