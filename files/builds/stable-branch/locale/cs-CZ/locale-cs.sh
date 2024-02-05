7f52def3238465dccd0ea4eedf5981c88351ab6a
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2024-02-05 16:06:24 +0100

    Update locale-cs.sh

--- files/builds/stable-branch/locale/cs-CZ/locale-cs.sh
+++ files/builds/stable-branch/locale/cs-CZ/locale-cs.sh
@@ -6,9 +6,9 @@
 # Author:       Steve Zabka                                                                        #
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
-# Copyright (c) 2020-2022                                                                          #
-# Time/Date:    19:00/26.07.2023                                                                   #
-# Version:      1.6.2                                                                              #
+# Copyright (c) 2020-2024                                                                          #
+# Time/Date:    16:00/05.02.2024                                                                   #
+# Version:      1.6.3                                                                              #
 ####################################################################################################
 
 # Path: /$HOME/.fusion360/locale/cs-CZ/locale-cs.sh
@@ -60,7 +60,7 @@ SP_LOGFILE_WINEPREFIX_INFO_TOOLTIP_3="Odstraňte aktuální Wineprefix ze svého
 SP_OS_TITLE="Linux distribuce - Konfigurace"
 SP_OS_LABEL_1="V tomto kroku nyní můžete vybrat svou distribuci Linuxu a nainstalovat potřebné balíčky pro instalaci."
 SP_OS_LABEL_2="Linux distribuce:"
-SP_OS_SELECT=$(echo "Arch Linux,Debian 11,Debian 12, Debian Testing,EndeavourOS,Fedora 37,Fedora 38,Fedora Rawhide,Linux Mint 20.x,Linux Mint 21.x,Linux Mint 5.x - LMDE Version,Manjaro Linux,openSUSE Leap 15.4,openSUSE Leap 15.5,openSUSE Tumbleweed,Red Hat Enterprise Linux 8.x,Red Hat Enterprise Linux 9.x,Solus,Ubuntu 20.04,Ubuntu 22.04,Ubuntu 23.04,Void Linux,Gentoo Linux")
+SP_OS_SELECT=$(echo "Arch Linux,Debian 11,Debian 12, Debian Testing,EndeavourOS,Fedora 38,Fedora 39,Fedora Rawhide,Linux Mint 20.x,Linux Mint 21.x,Linux Mint 5.x - LMDE Version,Manjaro Linux,openSUSE Leap 15.4,openSUSE Leap 15.5,openSUSE Tumbleweed,Red Hat Enterprise Linux 8.x,Red Hat Enterprise Linux 9.x,Solus,Ubuntu 20.04,Ubuntu 22.04,Ubuntu 23.04,Void Linux,Gentoo Linux")
 
 ###############################################################################################################################################################
 
@@ -83,7 +83,7 @@ SP_INSTALLDIR_INFO_LABEL_2="Vyberte prosím jiný adresář."
 SP_WINE_SETTINGS_TITLE="Vyberte verzi vína"
 SP_WINE_SETTINGS_LABEL_1="Zde se musíte rozhodnout mezi dvěma možnostmi*."
 SP_WINE_SETTINGS_LABEL_2="Vybrat:"
-SP_WINE_VERSION_SELECT=$(echo "Verze vína (Staging), Verze vína (6.23 nebo vyšší) je již nainstalována v systému!")
+SP_WINE_VERSION_SELECT=$(echo "Verze vína (Staging), Verze vína (8.14 nebo vyšší) je již nainstalována v systému!")
 SP_WINE_SETTINGS_LABEL_3="*V závislosti na zvolené možnosti se na váš systém nainstalují další balíčky!"
 
 ###############################################################################################################################################################
