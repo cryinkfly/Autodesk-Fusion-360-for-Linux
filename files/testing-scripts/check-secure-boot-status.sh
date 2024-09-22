a768f30de66d24f4a5270580affd19d8172d5d6c
Sumeet Dube <sumeetdube1234@gmail.com>
2024-09-22 18:09:56 +0530

    Added test case

--- files/testing-scripts/check-secure-boot-status.sh
+++ files/testing-scripts/check-secure-boot-status.sh
@@ -3,8 +3,8 @@
 # Function to check if Secure Boot is activated
 check_secure_boot() {
     if ! command -v mokutil &> /dev/null; then
-        echo "mokutil command not found. Please install it to check Secure Boot status."
-        return 1
+        echo "emokutil command not found. Please install it to check Secure Boot status."
+        exit 1
     fi
 
     # Check if Secure Boot is enabled
