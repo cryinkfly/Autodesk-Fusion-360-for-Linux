242e499edb2dfbb57f2810632e709df9fc71d05c
Steve Zabka <info@cryinkfly.de>
2025-06-15 10:24:58 +0200

    Update flatpak-installation.sh

--- files/builds/dev-branch/flatpak-installation.sh
+++ files/builds/dev-branch/flatpak-installation.sh
@@ -1,6 +1,9 @@
 # This script will install Autodesk Fusion in a Flatpak-Runtime!
 
 flatpak install flathub org.winehq.Wine -y #OR: flatpak install --user flathub org.winehq.Wine -y
+
+flatpak run org.winehq.Wine --version #Check version of wine
+
 flatpak run --env="WINEPREFIX=/home/$USER/.var/app/org.winehq.Wine/data/wineprefixes/fusion360" --command="winetricks" org.winehq.Wine -q sandbox
 flatpak run --env="WINEPREFIX=/home/$USER/.var/app/org.winehq.Wine/data/wineprefixes/fusion360" --command="winetricks" org.winehq.Wine -q sandbox atmlib gdiplus arial corefonts cjkfonts dotnet452 msxml4 msxml6 vcrun2017 fontsmooth=rgb winhttp win10
 WINEPREFIX="$SELECTED_DIRECTORY/wineprefix/autodesk_fusion" sh "winetricks" -q cjkfonts
